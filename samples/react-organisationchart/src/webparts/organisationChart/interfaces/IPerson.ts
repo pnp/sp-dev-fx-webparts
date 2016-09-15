a9fe43282ddf184d8a5541cb233730da8ba83dd3
Vardhaman Deshpande <vman@users.noreply.github.com>
2016-09-15 11:28:29 +0100

    Added the org chart web part (#15)
    * added the org chart web part
    * updated to drop 3, got batch from this.httpClient
    * updated comments
    * formatting
    * got user photos from /_layouts/15/userphoto.aspx
    * renamed folder, other fixes

---
+++ samples/react-organisationchart/src/webparts/organisationChart/interfaces/IPerson.ts
@@ -0,0 +1,8 @@
+export interface IPerson {
+  DirectReports?: string[];
+  PictureUrl?: string;
+  ExtendedManagers?: string[];
+  Title?: string;
+  DisplayName?: string;
+  PersonalUrl?: string;
+}
