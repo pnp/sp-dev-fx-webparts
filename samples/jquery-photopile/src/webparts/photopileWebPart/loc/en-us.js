d6fbb21a4dc3b264376c18713fb5357c8e6315d7
Vesa Juvonen <vesa.juvonen@hotmail.com>
2016-09-17 19:02:35 +0300

    Added Web Part from Olivier Carpentier around JQuery, Photopile.JS & Office UI Fabric Client-Side Web Part

---
+++ samples/jquery-photopile/src/webparts/photopileWebPart/loc/en-us.js
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * Photopile Web Part en-us string ressources
+ *
+ * Author: Olivier Carpentier
+ */
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Customize the photopile web part settings",
+    "ErrorSelectList": "Please edit the webpart and select a picture library. If the picture library dropdown list is empty, please create a new Picture Library in the current web site.",
+    "ErrorNoItems": "No items found in the current library. Please insert inside or select another library.",
+    "Loading": "Loading pictures...",
+    "PictureLibraryGroupName": "Library",
+    "ThumbnailsGroupName": "Thumbnails",
+    "PhotoContainerGroupName": "Photo container",
+    "AutoplayGroupName": "Autoplay",
+    "PictureLibraryFieldLabel": "Pictures library",
+    "OrderByFieldLabel": "Order by",
+    "OrderByChoiceLabelId": "ID",
+    "OrderByChoiceLabelTitle": "Title",
+    "OrderByChoiceLabelCreated": "Created",
+    "OrderByChoiceLabelModified": "Modified",
+    "OrderByChoiceLabelImageWidth": "Image width",
+    "OrderByChoiceLabelImageHeight": "Image height",
+    "OrderByAscFieldLabel": "Arranged",
+    "OrderByAscChoiceLabel": "Ascending",
+    "OrderByDescChoiceLabel": "Descending",
+    "PictureLibraryCountLabel": "Max count",
+    "NumLayersFieldLabel": "Number of layers in the pile (max zindex)",
+    "ThumbOverlabFieldLabel": "Overlap amount (px)",
+    "ThumbRotationFieldLabel": "Maximum rotation (deg)",
+    "ThumbBorderWidthFieldLabel": "Border width (px)",
+    "ThumbBorderColorFieldLabel": "Border color",
+    "ThumbBorderHoverFieldLabel": "Border hover color",
+    "DraggableFieldLabel": "Enable draggable thumbnails",
+    "FadeDurationFieldLabel": "Speed at which photo fades (ms)",
+    "PickupDurationFieldLabel": "Speed at which photo is picked up & put down (ms)",
+    "PhotoZIndexFieldLabel": "Z-Index (show above all)",
+    "PhotoBorderFieldLabel": "Border width around fullsize image",
+    "PhotoBorderColorFieldLabel": "Border color",
+    "ShowInfoFieldLabel": "Include photo description in photo container",
+    "AutoplayGalleryFieldLabel": "Autoplay the photopile",
+    "AutoplaySpeedFieldLabel": "Autoplay speed (ms)"
+  }
+});
