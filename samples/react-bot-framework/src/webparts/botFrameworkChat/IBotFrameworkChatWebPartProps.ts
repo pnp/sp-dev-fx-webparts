5eac8fee179dbc8b1ac4e9ae8f7128a2da0c3537
DESKTOP-K8O6TUG\gary <gary@garypretty.co.uk>
2016-10-11 00:52:23 +0100

    Initial commit of Bot Framework SPFX web part

---
+++ samples/react-bot-framework/src/webparts/botFrameworkChat/IBotFrameworkChatWebPartProps.ts
@@ -0,0 +1,13 @@
+export interface IBotFrameworkChatWebPartProps {
+  description: string;
+  message: string;
+  directLineSecret: string;
+  title: string;
+  placeholderText: string;
+  titleBarBackgroundColor : string;
+  botMessagesBackgroundColor: string;
+  botMessagesForegroundColor: string;
+  userMessagesBackgroundColor: string;
+  userMessagesForegroundColor: string;
+  directLineClientName: string;
+}
