590fd08b0c45ada4193d1c27388cf89bb2a19d95
Vesa Juvonen <vesa.juvonen@hotmail.com>
2016-10-28 15:46:57 +0300

    Updated pic and small adjustments on the code.

--- samples/react-bot-framework/src/webparts/botFrameworkChat/components/BotFrameworkChat.tsx
+++ samples/react-bot-framework/src/webparts/botFrameworkChat/components/BotFrameworkChat.tsx
@@ -21,52 +21,54 @@ export default class BotFrameworkChat extends React.Component<IBotFrameworkChatP
     return (
       <div className={styles.botFrameworkChat}>
         <div className={styles.container}>
-          <div className={css('ms-Grid-row ms-font-xl', styles.chatHeader) } style={{ backgroundColor: '#' + this.props.titleBarBackgroundColor }} >
-            { this.props.title }
+          <div className={css('ms-Grid-row ms-font-xl', styles.chatHeader)} style={{ backgroundColor: '#' + this.props.titleBarBackgroundColor }} >
+            {this.props.title}
           </div>
-          <div className={css('ms-Grid-row', styles.messagesRow) } >
+          <div className={css('ms-Grid-row', styles.messagesRow)} >
             <div className='ms-Grid-col ms-u-sm12' ref='messageHistoryDiv' dangerouslySetInnerHTML={{ __html: this.getMessagesHtml() }}>
             </div>
           </div>
-          <div className={css('ms-Grid-row') }>
-            <TextField id='MessageBox' onKeyUp={ (e) => this.tbKeyUp(e) } onKeyDown={ (e) => this.tbKeyDown(e) }
-              value={this.currentMessageText} placeholder={ this.props.placeholderText } className={ css('ms-fontSize-m', styles.messageBox) } />
+          <div className={css('ms-Grid-row')}>
+            <TextField id='MessageBox' onKeyUp={(e) => this.tbKeyUp(e)} onKeyDown={(e) => this.tbKeyDown(e)}
+              value={this.currentMessageText} placeholder={this.props.placeholderText} className={css('ms-fontSize-m', styles.messageBox)} />
           </div>
         </div>
       </div>
     );
   }
 
-  public componentDidMount(): void {
-    if (this.props.directLineSecret) {
-      var Swagger = require('swagger-client');
-      var directLineSpec = require('./directline-swagger.json');
+  public componentDidUpdate(prevProps: IBotFrameworkChatWebPartProps, prevState: {}, prevContext: any): void {
+    if (this.props.directLineSecret !== prevProps.directLineSecret) {
+      if (this.props.directLineSecret) {
+        var Swagger = require('swagger-client');
+        var directLineSpec = require('./directline-swagger.json');
 
-      this.directLineClientSwagger = new Swagger(
-        {
-          spec: directLineSpec,
-          usePromise: true,
-        }).then((client) => {
-          client.clientAuthorizations.add('AuthorizationBotConnector', new Swagger.ApiKeyAuthorization('Authorization', 'BotConnector ' + this.props.directLineSecret, 'header'));
-          console.log('DirectLine client generated');
-          return client;
-        }).catch((err) =>
-          console.error('Error initializing DirectLine client', err));
+        this.directLineClientSwagger = new Swagger(
+          {
+            spec: directLineSpec,
+            usePromise: true,
+          }).then((client) => {
+            client.clientAuthorizations.add('AuthorizationBotConnector', new Swagger.ApiKeyAuthorization('Authorization', 'BotConnector ' + this.props.directLineSecret, 'header'));
+            console.log('DirectLine client generated');
+            return client;
+          }).catch((err) =>
+            console.error('Error initializing DirectLine client', err));
 
-      this.directLineClientSwagger.then((client) => {
-        client.Conversations.Conversations_NewConversation()
-          .then((response) => response.obj.conversationId)
-          .then((conversationId) => {
+        this.directLineClientSwagger.then((client) => {
+          client.Conversations.Conversations_NewConversation()
+            .then((response) => response.obj.conversationId)
+            .then((conversationId) => {
 
-            this.conversationId = conversationId;
-            this.pollMessages(client, conversationId);
-            this.directLineClient = client;
-          });
-      });
+              this.conversationId = conversationId;
+              this.pollMessages(client, conversationId);
+              this.directLineClient = client;
+            });
+        });
 
-      this.sendAsUserName = this.props.context.pageContext.user.loginName;
+        this.sendAsUserName = this.props.context.pageContext.user.loginName;
 
-      this.printMessage = this.printMessage.bind(this);
+        this.printMessage = this.printMessage.bind(this);
+      }
     }
   }
 
