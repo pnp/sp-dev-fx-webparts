b77628f504c598c0ac157e0c1e484f42f693c989
Kevin Mees <kev.mees@gmail.com>
2016-11-04 13:04:00 +0100

    [react-mobx] scaffold project + migrate to drop5

---
+++ samples/react-mobx/src/webparts/reactMobx/ReactMobxWebPart.ts
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneSettings,
+  IWebPartContext,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'reactMobxStrings';
+import ReactMobx, { IReactMobxProps } from './components/ReactMobx';
+import { IReactMobxWebPartProps } from './IReactMobxWebPartProps';
+
+export default class ReactMobxWebPart extends BaseClientSideWebPart<IReactMobxWebPartProps> {
+
+  public constructor(context: IWebPartContext) {
+    super(context);
+  }
+
+  public render(): void {
+    const element: React.ReactElement<IReactMobxProps> = React.createElement(ReactMobx, {
+      description: this.properties.description
+    });
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected get propertyPaneSettings(): IPropertyPaneSettings {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
