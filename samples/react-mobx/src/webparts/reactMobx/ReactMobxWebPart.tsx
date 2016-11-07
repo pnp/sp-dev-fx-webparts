de79d221346b9c8639d5a39596f261fb6976ce65
Kevin Mees <kev.mees@gmail.com>
2016-11-07 10:32:36 +0100

    [react-mobx] only render the webpart once

--- samples/react-mobx/src/webparts/reactMobx/ReactMobxWebPart.tsx
+++ samples/react-mobx/src/webparts/reactMobx/ReactMobxWebPart.tsx
@@ -21,6 +21,7 @@ export default class ReactMobxWebPart extends BaseClientSideWebPart<IReactMobxWe
   }
 
   public render(): void {
+    if (this.renderedOnce) { return; }
 
     const element = (
       <Provider {...this.store}>
