35f2a6df96a161a57a70545827777948d0fdfffa
Aliaxei Terentiev <alexterentiev@Aliaxeis-MacBook-Pro.local>
2016-11-08 16:26:52 -0800

    initial commit

---
+++ samples/knockout-metadata/src/webparts/termSetRequester/TermSetRequesterWebPart.ts
@@ -0,0 +1,57 @@
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneSettings,
+  IWebPartContext,
+  PropertyPaneTextField
+} from '@microsoft/sp-client-preview';
+
+import styles from './TermSetRequester.module.scss';
+import * as strings from 'termSetRequesterStrings';
+import { ITermSetRequesterWebPartProps } from './ITermSetRequesterWebPartProps';
+
+export default class TermSetRequesterWebPart extends BaseClientSideWebPart<ITermSetRequesterWebPartProps> {
+
+  public constructor(context: IWebPartContext) {
+    super(context);
+  }
+
+  public render(): void {
+    this.domElement.innerHTML = `
+      <div class="${styles.termSetRequester}">
+        <div class="${styles.container}">
+          <div class="ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}">
+            <div class="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
+              <span class="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
+              <p class="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
+              <p class="ms-font-l ms-fontColor-white">${this.properties.description}</p>
+              <a href="https://github.com/SharePoint/sp-dev-docs/wiki" class="ms-Button ${styles.button}">
+                <span class="ms-Button-label">Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  protected get propertyPaneSettings(): IPropertyPaneSettings {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
