4b3b2811d280ba2692e870d360074a1a1fcfcf68
russellgove@gmail.com <russellgove@gmail.com>
2016-12-06 21:25:53 -0500

    gud

--- src/webparts/spfxReactGrid/components/SharePointFormatters.tsx
+++ src/webparts/spfxReactGrid/components/SharePointFormatters.tsx
@@ -3,13 +3,15 @@ import * as React from "react";
 export const SharePointLookupCellFormatter = React.createClass<{ value: string, onFocus: any,entityid:string, columnid:string }, void>({
   render(): JSX.Element {
     if (this.props.value == null) {
-      return ((<a href="#" onFocus={this.props.onFocus} key={this.props.entityid+this.props.columnid}>
+      return (<a href="#"  data-entityid={this.props.entityid}
+        data-columnid={this.props.columnid} onFocus={this.props.onFocus} key={this.props.entityid+this.props.columnid}>
 
-      </a>))
+      </a>)
     }
 
     let displayValue = this.props.value.substring(this.props.value.indexOf("#;") + 2);
-    return (<a href="#" onFocus={this.props.onFocus} key={this.props.entityid+this.props.columnid}>
+    return (<a href="#"  data-entityid={this.props.entityid}
+        data-columnid={this.props.columnid} onFocus={this.props.onFocus} key={this.props.entityid+this.props.columnid}>
       {displayValue}
     </a>)
 
