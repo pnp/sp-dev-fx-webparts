632143c1578f40a10e6f2b9d590bc51058d0f00a
Vesa Juvonen <vesa.juvonen@hotmail.com>
2017-02-11 02:18:59 +0200

    Cleaning old version of angular-search web part

--- samples/angular-search/src/webparts/angularSearch/IAngularSearchWebPartProps.ts
+++
@@ -1,4 +0,0 @@
-export interface IAngularSearchWebPartProps {
-  description: string;
-  contentTypes: string;
-}
--- samples/angular-search/src/webparts/angularSearch/tests/AngularSearch.test.ts
+++
@@ -1,7 +0,0 @@
-import * as assert from 'assert';
-
-describe('AngularSearchWebPart', () => {
-  it('should do something', () => {
-    assert.ok(true);
-  });
-});
