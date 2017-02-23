0855d8fb12848c3373eb1dae2a60471d57710183
Vesa Juvonen <vesa.juvonen@hotmail.com>
2017-02-23 15:12:33 +0200

    Deleting old tutorials to replace with GA versions

--- tutorials/helloworld-webpart/src/webparts/helloWorld/IHelloWorldWebPartProps.ts
+++
@@ -1,7 +0,0 @@
-export interface IHelloWorldWebPartProps {
-    description: string;
-    test: string;
-    test1: boolean;
-    test2: string;
-    test3: boolean;
-}
--- tutorials/jquery-webpart/src/webparts/jQuery/tests/JQuery.test.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="mocha" />
-
-import { assert } from 'chai';
-
-describe('JQueryWebPart', () => {
-  it('should do something', () => {
-    assert.ok(true);
-  });
-});
