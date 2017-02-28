81ef9f608d356b6c69d5a54c9a580e5ed1352d13
Patrick Rodgers <patrodg@microsoft.com>
2017-02-28 15:06:46 -0500

    Adding sample to demonstrate sp-pnp-js with SPFx GA

---
+++ samples/knockout-sp-pnp-js/src/webparts/spPnPjsExample/MockSpPnPjsExampleViewModel.ts
@@ -0,0 +1,85 @@
+import * as ko from 'knockout';
+import styles from './SpPnPjsExample.module.scss';
+import { ISpPnPjsExampleWebPartProps } from './ISpPnPjsExampleWebPartProps';
+import pnp, { List, ListEnsureResult, ItemAddResult } from "sp-pnp-js";
+import { ISpPnPjsExampleBindingContext, OrderListItem } from './SpPnPjsExampleViewModel';
+
+export default class MockSpPnPjsExampleViewModel {
+
+    public description: KnockoutObservable<string> = ko.observable('');
+    public newItemTitle: KnockoutObservable<string> = ko.observable('');
+    public newItemNumber: KnockoutObservable<string> = ko.observable('');
+    public items: KnockoutObservableArray<OrderListItem> = ko.observableArray([]);
+
+    public labelClass: string = styles.label;
+    public helloWorldClass: string = styles.helloWorld;
+    public containerClass: string = styles.container;
+    public rowClass: string = `ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}`;
+    public buttonClass: string = `ms-Button ${styles.button}`;
+
+    constructor(bindings: ISpPnPjsExampleBindingContext) {
+        this.description(bindings.description);
+
+        // When web part description is updated, change this view model's description.
+        bindings.shouter.subscribe((value: string) => {
+            this.description(value);
+        }, this, 'description');
+
+        // call the load the items
+        this.getItems().then(items => {
+
+            this.items(items);
+        });
+    }
+
+    /**
+     * Gets the items from the list
+     */
+    private getItems(): Promise<OrderListItem[]> {
+        return Promise.resolve([{
+            Id: 1,
+            Title: "Mock Item 1",
+            OrderNumber: "12345"
+        },
+        {
+            Id: 2,
+            Title: "Mock Item 2",
+            OrderNumber: "12345"
+        },
+        {
+            Id: 3,
+            Title: "Mock Item 3",
+            OrderNumber: "12345"
+        }]);
+    }
+
+    /**
+     * Adds an item to the list
+     */
+    public addItem(): void {
+
+        if (this.newItemTitle() !== "" && this.newItemNumber() !== "") {
+
+            // add the new item to the display
+            this.items.push({
+                Id: this.items.length,
+                OrderNumber: this.newItemNumber(),
+                Title: this.newItemTitle(),
+            });
+
+            // clear the form
+            this.newItemTitle("");
+            this.newItemNumber("");
+        }
+    }
+
+    /**
+     * Deletes an item from the list
+     */
+    public deleteItem(data): void {
+
+        if (confirm("Are you sure you want to delete this item?")) {
+            this.items.remove(data);
+        }
+    }
+}
