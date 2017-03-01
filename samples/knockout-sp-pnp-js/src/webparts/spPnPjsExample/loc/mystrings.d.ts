0e7aabce15b1b35f90ddbcebfb44214fc09add72
Patrick Rodgers <patrick.rodgers@microsoft.com>
2017-03-01 02:35:09 -0500

    Adding sample to demonstrate sp-pnp-js with SPFx GA (#136)

---
+++ samples/knockout-sp-pnp-js/src/webparts/spPnPjsExample/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISpPnPjsExampleStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'spPnPjsExampleStrings' {
+  const strings: ISpPnPjsExampleStrings;
+  export = strings;
+}
