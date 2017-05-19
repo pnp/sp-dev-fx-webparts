dbd7d66ff60ed18bca4f3754447bfe089dd3d9d1
Waldek Mastykarz <waldek@mastykarz.nl>
2017-05-19 15:40:43 +0200

    Added sample project with API secured with AAD (#207)
    Moved web part to a subdirectory

---
+++ samples/aad-api-spo-cookie/api/pnp.api.contosoorders/App_Start/WebApiConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Http;
+
+namespace pnp.api.contosoorders {
+    public static class WebApiConfig {
+        public static void Register(HttpConfiguration config) {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.EnableCors();
+            config.Filters.Add(new AuthorizeAttribute());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ samples/aad-api-spo-cookie/api/pnp.api.contosoorders/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pnp.api.contosoorders.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
