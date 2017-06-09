d16eacd0d0efc11ea772c0053265d314d7df8d03
Gautam Sheth <gautam.sheth@zevenseas.com>
2017-06-09 18:02:12 +0530

    Upgraded angular-search to GA
    Fixed typo, minor change to httpclient response

---
+++ samples/gulpfile.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const gulp = require('gulp');
+const build = require('@microsoft/sp-build-web');
+
+build.initialize(gulp);
---
+++ samples/src/webparts/angularSearch/AngularSearch.module.scss.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+require('./AngularSearch.module.css');
+const styles = {
+  row: 'row_10ef10ef',
+  column: 'column_10ef10ef',
+  title: 'title_10ef10ef',
+  subtitle: 'subtitle_10ef10ef',
+  description: 'description_10ef10ef',
+  button: 'button_10ef10ef',
+  'ms-ListItem': 'ms-ListItem_10ef10ef',
+};
+
+export default styles;
+/* tslint:enable */
