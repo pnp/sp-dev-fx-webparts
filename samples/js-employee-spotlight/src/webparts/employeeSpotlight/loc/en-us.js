f2e8d2d1f96f3782133e526d0ecfe1a73678a571
Ravi Kumar Bomma <brk114@gmail.com>
2017-06-20 13:32:02 +0530

    Added spfx sample for Employee Spotlight webpart
    Added the sample webpart code to display the Employee Spotlight details
    from a configured list using javascript and SharePoint framework

---
+++ samples/js-employee-spotlight/src/webparts/employeeSpotlight/loc/en-us.js
@@ -0,0 +1,15 @@
+define([], function() {
+    return {
+        "propertyPaneHeading": "Spotlight Configuration",
+        "selectSiteLableMessage": "Select Site",
+        "selectListLableMessage": "Select List Name",
+        "employeeEmailcolumnLableMessage": "Select Employee Name column",
+        "descriptioncolumnLableMessage": "Select Description column",
+        "expirationDateColumnLableMessage": "Select ExpiryDate column",
+        "effectsGroupName": "Slider Effects",
+        "spotlightBGColorLableMessage": "Background Color",
+        "spotlightFontColorLableMessage": "Font Color",
+        "enableAutoSlideLableMessage": "Enable auto slide",
+        "carouselSpeedLableMessage": "Slider speed(in seconds)"
+    }
+});
