a6e93369d00fd2369137f16539e70c34b91fec64
vman <vardhaman.rd@gmail.com>
2017-07-10 01:46:47 +0100

    added wp base

---
+++ samples/react-redux-restapi/src/webparts/demo/DemoWebPart.ts
@@ -0,0 +1,53 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'demoStrings';
+import Demo from './components/Demo';
+import { IDemoProps } from './components/IDemoProps';
+import { IDemoWebPartProps } from './IDemoWebPartProps';
+
+export default class DemoWebPart extends BaseClientSideWebPart<IDemoWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IDemoProps > = React.createElement(
+      Demo,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
