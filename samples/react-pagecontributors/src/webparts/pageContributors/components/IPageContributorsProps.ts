3785f1e2dee259eda4af11493fdc9cb1e6be75d6
Stéphane MAGNE <smagne@mcnext.com>
2017-07-26 00:27:09 +0200

    Added page contributors webpart

---
+++ samples/react-pagecontributors/src/webparts/pageContributors/components/IPageContributorsProps.ts
@@ -0,0 +1,7 @@
+import { PersonaSize } from "office-ui-fabric-react/lib";
+
+export interface IPageContributorsProps {
+    personaSize: PersonaSize;
+    numberOfFaces: number;
+    pageUrl: string;
+}
