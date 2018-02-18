cb9f8cb51a5151c4baaed054364ef6b8cc768354
BuckyAustin <buckyaustin@gmail.com>
2018-02-18 00:30:25 +0000

    Added OfflineHTTPService implementation to file

--- samples/react-offline-first/src/webparts/reactOfflineFirst/components/ReactOfflineFirst.tsx
+++ samples/react-offline-first/src/webparts/reactOfflineFirst/components/ReactOfflineFirst.tsx
@@ -2,30 +2,54 @@ import * as React from 'react';
 import styles from './ReactOfflineFirst.module.scss';
 import { IReactOfflineFirstProps } from './IReactOfflineFirstProps';
 import { escape } from '@microsoft/sp-lodash-subset';
-import * as localforage from "localforage";
+import { IReactOfflineFirstState } from './IReactOfflineFirstState';
+import { IOfflineStorageRequest } from '../services/IOfflineStorageRequest';
+import { IOfflineStorageItem } from '../services/IOffllineStorageItem';
+import { OfflineFirstHTTPService } from '../services/OfflineFirstHTTPService';
 
-export default class ReactOfflineFirst extends React.Component<IReactOfflineFirstProps, {}> {
+export default class ReactOfflineFirst extends React.Component<IReactOfflineFirstProps, IReactOfflineFirstState> {
+  private offlineHTTP: OfflineFirstHTTPService;
+
+  public constructor(props: IReactOfflineFirstProps) {
+    super(props);
+    window['disableBeaconLogToConsole'] = true;
+    this.state = {
+      listOfGitHubRepos : []
+    } as IReactOfflineFirstState;
+    this.offlineHTTP = new OfflineFirstHTTPService();
+  }
 
   public componentDidMount(): void {
-    localforage.ready()
-    .then((event: any) => {
-      fetch("https://api.github.com/orgs/SharePoint/repos")
-      .then((response: Response) => {
-        if(response.ok) {
-          response.json()
-          .then((json: any) => {
-            localforage.setItem("https://api.github.com/orgs/SharePoint/repos", json);
-          })
-          .catch((error: Error|any) => {
-            console.error(error);
-          });
-        }
-      })
-      .catch((error: Error|any) => {
-        console.error(error);
+    const demoUrl: string = "https://api.github.com/orgs/SharePoint/repos";
+    const demoRequest: RequestInfo = {
+      url: demoUrl
+    } as RequestInfo;
+    const demoOfflineRequest: IOfflineStorageRequest = {key: "demoGet", value:{ requestInfo: demoRequest, requestInit: null}} as IOfflineStorageRequest;
+
+    this.offlineHTTP.getFromLocal(demoOfflineRequest.key)
+    .then( (offlineItem: any) => {
+      this.setState({listOfGitHubRepos: offlineItem},
+        () => {
+          console.log('retrieved items from offline.');
+          //set state callback.
+          this.componentDidMountWithLocal(demoOfflineRequest);
       });
     })
-    .catch((error: Error | any) => {
+    .catch((error: Error|any) => {
+      console.error(error);
+    });
+  }
+
+  private componentDidMountWithLocal(demoOfflineRequest):void {
+    this.offlineHTTP.getFromServer(demoOfflineRequest)
+    .then( (onlineItem: any) => {
+      this.setState({listOfGitHubRepos: onlineItem},
+        () => {
+          console.log('retrieved items from online.');
+          //set state callback.
+      });
+    })
+    .catch((error: Error|any) => {
       console.error(error);
     });
   }
@@ -42,6 +66,16 @@ export default class ReactOfflineFirst extends React.Component<IReactOfflineFirs
               <a href="https://aka.ms/spfx" className={ styles.button }>
                 <span className={ styles.label }>Learn more</span>
               </a>
+              {
+                this.state.listOfGitHubRepos &&
+                this.state.listOfGitHubRepos.map((repo) => {
+                  return (
+                    <div>
+                      {repo.full_name}
+                    </div>
+                  );
+                })
+              }
             </div>
           </div>
         </div>
