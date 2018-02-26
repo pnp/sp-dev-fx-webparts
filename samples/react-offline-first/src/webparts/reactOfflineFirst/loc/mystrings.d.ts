44189550bf2541aebeadac89e42072e528c35499
Austin Breslin <buckyaustin@gmail.com>
2018-02-26 08:30:58 +0000

    Offline first react (#421)
    * initial commit
    
    * Added OfflineHTTPService implementation to file
    
    * Put interfaces into their own files
    
    * Create a state object to allow the render method to have a variable to print.
    
    * Tested Gets, need to update comments
    
    * Add JSDoc
    
    * Created Readme.md

---
+++ samples/react-offline-first/src/webparts/reactOfflineFirst/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IReactOfflineFirstWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ReactOfflineFirstWebPartStrings' {
+  const strings: IReactOfflineFirstWebPartStrings;
+  export = strings;
+}
