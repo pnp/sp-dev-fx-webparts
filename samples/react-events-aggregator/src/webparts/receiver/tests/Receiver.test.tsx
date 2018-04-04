4acdd442bd791a311da872f66125a6b08e275f4e
Velin Georgiev <velin.georgiev@gmail.com>
2018-04-04 15:52:02 +0100

    react events aggregator updated to version 1.4.1 of the SPFx (#455)
    Readme updated

--- samples/react-events-aggregator/src/webparts/receiver/tests/Receiver.test.tsx
+++ samples/react-events-aggregator/src/webparts/receiver/tests/Receiver.test.tsx
@@ -2,38 +2,45 @@
 /// <reference types="sinon" />
 
 import * as React from 'react';
-import { expect } from 'chai';
+import { assert } from 'chai';
 import { mount, ReactWrapper } from 'enzyme';
 import { IEventAggregator, IEvent } from "@microsoft/sp-webpart-base/lib";
 import EventAggregator from "@microsoft/sp-webpart-base/lib/core/events/EventAggregator";
 import Receiver from "../components/Receiver";
 import { EventData } from "../../../sharedLibs/EventData";
 
-declare const sinon: any;
+declare const sinon: sinon.SinonStatic;
 
 describe('ReceiverWebPart', () => {
 
-  let subscribeByEventNameSpy: any;
-  let subscribeBySourceIdSpy: any;
-  let receivedEventSpy: any;
+  let subscribeByEventNameSpy: sinon.SinonSpy;
+  let subscribeBySourceIdSpy: sinon.SinonSpy;
+  let receivedEventSpy: sinon.SinonSpy;
   let receiver: ReactWrapper<Receiver, any>;
   let eventAggregator: IEventAggregator = new EventAggregator();
 
-  before(() => {
+  beforeEach(() => {
 
     // create spies so we test if event is triggered.
     subscribeByEventNameSpy = sinon.spy(EventAggregator.prototype, "subscribeByEventName");
     subscribeBySourceIdSpy = sinon.spy(EventAggregator.prototype, "subscribeBySourceId");
-    receivedEventSpy = sinon.spy(Receiver.prototype, "receivedEvent");
+    receivedEventSpy = sinon.spy((Receiver.prototype as any), "receivedEvent");
 
     // mount the Receiver so we can test it.
     receiver = mount(<Receiver eventAggregator={eventAggregator} subscriberId="123" />);
   });
 
+  afterEach(()=>{
+    receiver.unmount();
+    subscribeByEventNameSpy.restore();
+    subscribeBySourceIdSpy.restore();
+    receivedEventSpy.restore();
+  });
+
   it('should Receiver be subscribed by event name', () => {
 
-    expect(subscribeByEventNameSpy.calledOnce).to.be.true;
-    expect(subscribeByEventNameSpy.calledWith("myCustomEvent:start", "123", sinon.match.any)).to.be.true;
+    assert(subscribeByEventNameSpy.calledOnce === true);
+    assert(subscribeByEventNameSpy.calledWith("myCustomEvent:start", "123", sinon.match.any) === true);
   });
 
   it('should Receive an event and call the receivedEvent function', () => {
@@ -49,7 +56,7 @@ describe('ReceiverWebPart', () => {
     // raiseEvent so we can test if it has been received by the receivedEvent function.
     eventAggregator.raiseEvent(eventName, eventObject);
 
-    expect(receivedEventSpy.calledOnce).to.be.true;
-    expect(receivedEventSpy.calledWith(eventName, eventObject)).to.be.true;
+    assert(receivedEventSpy.calledOnce === true);
+    assert(receivedEventSpy.calledWith(eventName, eventObject) === true);
   });
 });
