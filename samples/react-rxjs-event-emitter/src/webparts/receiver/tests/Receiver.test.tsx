9c5b2d1adea0cede470bb117e135f74bc7a2850b
Velin Georgiev <velin.georgiev@gmail.com>
2018-04-20 23:50:19 +0100

    updated to 1.4.1
    tests updated, package removed

--- samples/react-rxjs-event-emitter/src/webparts/receiver/tests/Receiver.test.tsx
+++ samples/react-rxjs-event-emitter/src/webparts/receiver/tests/Receiver.test.tsx
@@ -1,24 +1,23 @@
 /// <reference types="mocha" />
 /// <reference types="sinon" />
-
 import * as React from 'react';
-import { expect } from 'chai';
+import { assert } from 'chai';
 import { mount, ReactWrapper } from 'enzyme';
 import Receiver from "../components/Receiver";
 
 import { EventData } from "../../../libraries/rxJsEventEmitter/EventData";
 import { RxJsEventEmitter } from "../../../libraries/rxJsEventEmitter/RxJsEventEmitter";
 
-declare const sinon: any;
+declare const sinon: sinon.SinonStatic;
 
 describe('ReceiverWebPart', () => {
 
-  let rxJsEventEmitterOnSpy: any;
-  let receivedEventSpy: any;
+  let rxJsEventEmitterOnSpy: sinon.SinonSpy;
+  let receivedEventSpy: sinon.SinonSpy;
   let receiver: ReactWrapper<Receiver, any>;
   let eventEmitter: RxJsEventEmitter = RxJsEventEmitter.getInstance();
 
-  before(() => {
+  beforeEach(() => {
 
     // create spies so we test if event is triggered.
     rxJsEventEmitterOnSpy = sinon.spy(RxJsEventEmitter.prototype, "on");
@@ -28,10 +27,16 @@ describe('ReceiverWebPart', () => {
     receiver = mount(<Receiver />);
   });
 
+  afterEach(() => {
+    receiver.unmount();
+    rxJsEventEmitterOnSpy.restore();
+    receivedEventSpy.restore();
+  });
+
   it('should Receiver be subscribed by event name', () => {
 
-    expect(rxJsEventEmitterOnSpy.calledOnce).to.be.true;
-    expect(rxJsEventEmitterOnSpy.calledWith("myCustomEvent:start", sinon.match.any)).to.be.true;
+    assert(rxJsEventEmitterOnSpy.calledOnce === true);
+    assert(rxJsEventEmitterOnSpy.calledWith("myCustomEvent:start", sinon.match.any) === true);
   });
 
   it('should Receive an event and call the receivedEvent function', () => {
@@ -41,7 +46,7 @@ describe('ReceiverWebPart', () => {
     // emit the data so we can test if it has been received by the receivedEvent function.
     eventEmitter.emit("myCustomEvent:start", data);
 
-    expect(receivedEventSpy.calledOnce).to.be.true;
-    expect(receivedEventSpy.calledWith(data)).to.be.true;
+    assert(receivedEventSpy.calledOnce === true);
+    assert(receivedEventSpy.calledWith(data) === true);
   });
 });
