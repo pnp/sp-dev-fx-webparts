9c52d8e41443af999bda55960edc3a9d30d58359
Velin Georgiev <velin.georgiev@gmail.com>
2018-05-10 22:17:30 +0100

    Adds SPFx React Jest Testing sample

---
+++ samples/react-jest-testing/src/webparts/iceCreamShop/components/IIceCreamShopProps.ts
@@ -0,0 +1,6 @@
+import { IIceCreamProvider } from "../iceCreamProviders/IIceCreamProvider";
+
+export interface IIceCreamShopProps {
+  iceCreamProvider: IIceCreamProvider;
+  strings: IIceCreamShopWebPartStrings;
+}
