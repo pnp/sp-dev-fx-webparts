332b918418be5086c185160fd34b467f0cec5c19
Gove <Russell.Gove@tronox.com>
2018-06-07 12:51:31 -0400

    initial version

---
+++ samples/react-item-History/src/extensions/itemHistory/ItemHistoryCommandSet.ts
@@ -0,0 +1,56 @@
+import { override } from '@microsoft/decorators';
+import { Log } from '@microsoft/sp-core-library';
+import {
+  BaseListViewCommandSet,
+  Command,
+  IListViewCommandSetListViewUpdatedParameters,
+  IListViewCommandSetExecuteEventParameters
+} from '@microsoft/sp-listview-extensibility';
+import { Dialog } from '@microsoft/sp-dialog';
+
+import * as strings from 'ItemHistoryCommandSetStrings';
+
+/**
+ * If your command set uses the ClientSideComponentProperties JSON input,
+ * it will be deserialized into the BaseExtension.properties object.
+ * You can define an interface to describe it.
+ */
+export interface IItemHistoryCommandSetProperties {
+  // This is an example; replace with your own properties
+  sampleTextOne: string;
+  sampleTextTwo: string;
+}
+
+const LOG_SOURCE: string = 'ItemHistoryCommandSet';
+
+export default class ItemHistoryCommandSet extends BaseListViewCommandSet<IItemHistoryCommandSetProperties> {
+
+  @override
+  public onInit(): Promise<void> {
+    Log.info(LOG_SOURCE, 'Initialized ItemHistoryCommandSet');
+    return Promise.resolve();
+  }
+
+  @override
+  public onListViewUpdated(event: IListViewCommandSetListViewUpdatedParameters): void {
+    const compareOneCommand: Command = this.tryGetCommand('COMMAND_1');
+    if (compareOneCommand) {
+      // This command should be hidden unless exactly one row is selected.
+      compareOneCommand.visible = event.selectedRows.length === 1;
+    }
+  }
+
+  @override
+  public onExecute(event: IListViewCommandSetExecuteEventParameters): void {
+    switch (event.itemId) {
+      case 'COMMAND_1':
+        Dialog.alert(`${this.properties.sampleTextOne}`);
+        break;
+      case 'COMMAND_2':
+        Dialog.alert(`${this.properties.sampleTextTwo}`);
+        break;
+      default:
+        throw new Error('Unknown command');
+    }
+  }
+}
