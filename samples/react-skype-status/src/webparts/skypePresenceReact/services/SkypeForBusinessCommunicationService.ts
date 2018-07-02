415b771e86472eb7458405a172501e8b8cb8e511
Vincent Biret <vincentbiret@hotmail.com>
2018-07-02 03:09:48 -0400

    upgrading react-skype-status to spfx 1.5 (#547)
    * react-skype-status: upgrading to spfx v1.5
    
    * react-skype-status: fixing unexisting referenced module
    
    * react-skype-status: upgrading to office ui fabric react 6.X
    
    * react-skype-status: switching to production infrastructure ids

--- samples/react-skype-status/src/webparts/skypePresenceReact/services/SkypeForBusinessCommunicationService.ts
+++ samples/react-skype-status/src/webparts/skypePresenceReact/services/SkypeForBusinessCommunicationService.ts
@@ -50,8 +50,8 @@ export class SkypeForBusinessCommunicationService implements ICommunicationServi
                 return SkypeForBusinessCommunicationService.configurationService.getCurrentConfiguration().then((currentConfiguration) => {
                     return jQuery.getScript("https://swx.cdn.skype.com/shared/v/1.2.36/SkypeBootstrap.min.js").then(() => {
                         const config: {apiKey: string, apiKeyCC: string} = {
-                            apiKey: "a42fcebd-5b43-4b89-a065-74450fb91255", // sdk
-                            apiKeyCC: "9c967f6b-a846-4df2-b43d-5167e47d81e1", // sdk+ui
+                          apiKey: '595a1aeb-e6dc-4e5b-be96-bb38adc83da1', // SDK
+                          apiKeyCC: '08c97289-7d57-404f-bd97-a6047403e370', // SDK+UI
                         };
                         if (currentConfiguration && currentConfiguration.ClientId) {
                             Skype.initialize({ apiKey: config.apiKey }, (api) => {
