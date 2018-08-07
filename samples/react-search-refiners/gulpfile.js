b7d6ec09d833f169383621e269a8bfd260cc710a
Mikael Svenson <miksvenson@gmail.com>
2018-08-07 10:22:44 +0200

    Added webpack-bundle-analyzer

--- samples/react-search-refiners/gulpfile.js
+++ samples/react-search-refiners/gulpfile.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const gulp = require('gulp');
+const path = require('path');
 const build = require('@microsoft/sp-build-web');
+const bundleAnalyzer = require('webpack-bundle-analyzer');
 
 /********************************************************************************************
  * Adds an alias for handlebars in order to avoid errors while gulping the project
@@ -27,9 +29,21 @@ build.configureWebpack.mergeConfig({
     generatedConfiguration.node = {
       fs: 'empty'
     }
+
+    const lastDirName = path.basename(__dirname);
+      const dropPath = path.join(__dirname, 'temp', 'stats');
+      generatedConfiguration.plugins.push(new bundleAnalyzer.BundleAnalyzerPlugin({
+        openAnalyzer: false,
+        analyzerMode: 'static',
+        reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
+        generateStatsFile: true,
+        statsFilename: path.join(dropPath, `${lastDirName}.stats.json`),
+        logLevel: 'error'
+      }));
     
     return generatedConfiguration;
   }
 });
+
 
 build.initialize(gulp);
