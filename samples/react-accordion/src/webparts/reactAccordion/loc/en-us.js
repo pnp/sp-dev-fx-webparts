b388efc070b15d4646afd3978a0fe53e7f3c9bd3
Gautam Sheth <gautam.sheth@zevenseas.com>
2018-08-17 18:35:46 +0530

    React Accordion Webpart added

---
+++ samples/react-accordion/src/webparts/reactAccordion/loc/en-us.js
@@ -0,0 +1,8 @@
+define([], function () {
+  return {
+    "PropertyPaneDescription": "Description",
+    "BasicGroupName": "Group Name",
+    "ListNameLabel": "List Name",
+    "MaxItemsPerPageLabel": "Max number of items per page"
+  }
+});
