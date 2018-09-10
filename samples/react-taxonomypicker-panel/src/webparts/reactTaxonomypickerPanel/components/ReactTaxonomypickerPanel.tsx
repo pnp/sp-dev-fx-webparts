274801e2fcce5fa18bb8902be82e657e61bb0a57
Gautam Sheth <gautam.sheth@zevenseas.com>
2018-09-10 15:40:38 +0530

    Taxonomy picker - version upgrade and bug fix (#601)

--- samples/react-taxonomypicker-panel/src/webparts/reactTaxonomypickerPanel/components/ReactTaxonomypickerPanel.tsx
+++ samples/react-taxonomypicker-panel/src/webparts/reactTaxonomypickerPanel/components/ReactTaxonomypickerPanel.tsx
@@ -1,71 +1,70 @@
 import * as React from 'react';
 import styles from './ReactTaxonomypickerPanel.module.scss';
 import { IReactTaxonomypickerPanelProps } from './IReactTaxonomypickerPanelProps';
-import { escape } from '@microsoft/sp-lodash-subset';
 
 // Controls
 import TermsPickerComponent, { ITaxonomyTerm } from './TermsPickerComponent';
 import { DefaultButton, IButtonProps, Button } from 'office-ui-fabric-react/lib/Button';
 
-export interface ITaxonomyPickerWebpartState{
-  SingleSelectFieldTerms:ITaxonomyTerm[],
-  MultiSelectFieldTerms:ITaxonomyTerm[]
+export interface ITaxonomyPickerWebpartState {
+  SingleSelectFieldTerms: ITaxonomyTerm[],
+  MultiSelectFieldTerms: ITaxonomyTerm[]
 }
 
 export default class ReactTaxonomypickerPanel extends React.Component<IReactTaxonomypickerPanelProps, ITaxonomyPickerWebpartState> {
-  constructor(props, state:ITaxonomyPickerWebpartState){
+  constructor(props, state: ITaxonomyPickerWebpartState) {
     super(props);
 
     this.state = {
-      SingleSelectFieldTerms:[],
-      MultiSelectFieldTerms:[]
+      SingleSelectFieldTerms: [],
+      MultiSelectFieldTerms: []
 
       // Supply array in the below format for a pre-populated control.
       //SingleSelectFieldTerms:[{name:"<Term-Label>", key="<Term-GUID>"}],
       //MultiSelectFieldTerms:[{name:"<Term-Label>", key="<Term-GUID>"}, {name:"<Term-Label>", key="<Term-GUID>"}]
     }
-    
- }
+
+  }
 
   public render(): React.ReactElement<IReactTaxonomypickerPanelProps> {
     return (
       <div>
-          <div className="ms-Grid">
-            <div className="ms-Grid-row">
-              <div className="ms-Grid-col ms-sm12 ms-lg12">
-                  <TermsPickerComponent IsMultiValue={false} TermSetId='<TERM-SET-ID>' LabelText='Single-select field' SelectedTerms={this.state.SingleSelectFieldTerms}/>
-              </div>
-            </div>
-            <div className="ms-Grid-row">
-              <div className="ms-Grid-col ms-sm12 ms-lg12">
-                  <TermsPickerComponent IsMultiValue={true} TermSetId='<TERM-SET-ID>' LabelText='Multi-select field' SelectedTerms={this.state.MultiSelectFieldTerms}/>
-              </div>
+        <div className="ms-Grid">
+          <div className="ms-Grid-row">
+            <div className="ms-Grid-col ms-sm12 ms-lg12">
+              <TermsPickerComponent IsMultiValue={false} TermSetId='<TERM-SET-ID>' LabelText='Single-select field' SelectedTerms={this.state.SingleSelectFieldTerms} />
             </div>
           </div>
+          <div className="ms-Grid-row">
+            <div className="ms-Grid-col ms-sm12 ms-lg12">
+              <TermsPickerComponent IsMultiValue={true} TermSetId='<TERM-SET-ID>' LabelText='Multi-select field' SelectedTerms={this.state.MultiSelectFieldTerms} />
+            </div>
+          </div>
+        </div>
         <div>
-    </div>
-    <br/>
-    <br/>
+        </div>
+        <br />
+        <br />
         <DefaultButton
-            primary={true}
-            text="Show selected values"
-            onClick={this._showTaxonomyControlValues.bind(this)}
+          primary={true}
+          text="Show selected values"
+          onClick={this._showTaxonomyControlValues.bind(this)}
         />
-     </div>
+      </div>
     );
   }
 
-  private _showTaxonomyControlValues(){
+  private _showTaxonomyControlValues() {
 
-    if(this.state.SingleSelectFieldTerms.length > 0){
-        alert("Single-Select term Label and GUID : \n" + this.state.SingleSelectFieldTerms[0].name + " - " + this.state.SingleSelectFieldTerms[0].key);
+    if (this.state.SingleSelectFieldTerms.length > 0) {
+      alert("Single-Select term Label and GUID : \n" + this.state.SingleSelectFieldTerms[0].name + " - " + this.state.SingleSelectFieldTerms[0].key);
     }
-    if(this.state.MultiSelectFieldTerms.length > 0){
-        let multiSelectValues = this.state.MultiSelectFieldTerms.map(trm => {
-          return trm.name + " - " + trm.key
-        }).join(' | ');
+    if (this.state.MultiSelectFieldTerms.length > 0) {
+      let multiSelectValues = this.state.MultiSelectFieldTerms.map(trm => {
+        return trm.name + " - " + trm.key
+      }).join(' | ');
 
-        alert("Multi-select term Label and GUID : \n" + multiSelectValues); 
+      alert("Multi-select term Label and GUID : \n" + multiSelectValues);
     }
   }
 }
