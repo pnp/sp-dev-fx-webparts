00217308c4ba947913763f268341c9dcc5c8eeef
Nanddeep Nachan <nanddeep_nachan@hotmail.com>
2018-11-28 11:50:05 +0530

    Added sample react-adaptive-cards-image-gallery

---
+++ samples/react-adaptive-cards-image-gallery/src/webparts/adaptiveCardsImageGallery/loc/mystrings.d.ts
@@ -0,0 +1,11 @@
+declare interface IAdaptiveCardsImageGalleryWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  ImageGalleryNameFieldLabel: string;
+  ImagesToDisplayFieldLabel: string;
+}
+
+declare module 'AdaptiveCardsImageGalleryWebPartStrings' {
+  const strings: IAdaptiveCardsImageGalleryWebPartStrings;
+  export = strings;
+}
