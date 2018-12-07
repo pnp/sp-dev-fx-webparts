a14611bddce1fc83b7dfd0e7c553593056bfc7bb
Franck Cornu <franck.cornu@outlook.com>
2018-12-07 10:20:07 -0500

    * Added result type interface

---
+++ samples/react-search-refiners/spfx/src/models/ISearchResultType.ts
@@ -0,0 +1,23 @@
+﻿export interface ISearchResultType {
+    condition: ISearchResulTypeCondition;
+    templateContent: string;
+}
+
+export enum ResultTypeOperator {
+    Equal,
+    LessThan,
+    GreaterThan,
+    LessOrEqual,
+    GreaterOrEqual
+}
+
+export interface ISearchResulTypeCondition {
+    property: string;
+    operator: ResultTypeOperator;
+    value: string;
+}
+
+export enum ResultTypeEditOption {
+    External,
+    Inline
+}
