c4f71bc15869a687f0358d3aeb89649513dcc73f
joaojmendes <joao.j.mendes@outlook.com>
2018-12-28 15:47:35 +0000

    React-Birthdays Web Part (#716)
    * Birthday Web Part
    
    * commit change
    
    * commit chnages
    
    * commit changes
    
    * commit changes
    
    * commit changes
    
    * commit changes
    
    * commit changes
    
    * Update index.js

---
+++ samples/react-birthdays/src/webparts/Birthdays/loc/en-us.js
@@ -0,0 +1,8 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Happy Brithday",
+    "BasicGroupName": "Properties",
+    "DescriptionFieldLabel": "Title",
+    "NumberUpComingDaysLabel": 'Number of upcomming birthdays'
+  }
+});
