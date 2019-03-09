b5331ed08dffced4aae8c4770cd2a1ba6d1f6aca
Joel Rodrigues <19577724+joelfmrodrigues@users.noreply.github.com>
2019-03-09 10:39:42 +0000

    React my teams (#795)
    * Add my teams react web part
    
    * update file extension
    
    * add missing api permissions

---
+++ samples/react-my-teams/src/shared/interfaces/IChannel.ts
@@ -0,0 +1,5 @@
+export interface IChannel {
+  id: string;
+  displayName: string;
+  description: string;
+}
---
+++ samples/react-my-teams/src/shared/interfaces/index.ts
@@ -0,0 +1,3 @@
+export * from './ITenant';
+export * from './ITeam';
+export * from './IChannel';
