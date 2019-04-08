01401ce5d4ed4b8f7ac91e306312c1ba904c7f47
Joel Rodrigues <joel.rodrigues@cps.co.uk>
2019-04-08 16:02:05 +0100

    add solution

---
+++ samples/workbench-customizer/src/webparts/workbenchCustomizer/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IWorkbenchCustomizerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  CustomWorkbenchStylesFieldLabel: string;
+}
+
+declare module 'WorkbenchCustomizerWebPartStrings' {
+  const strings: IWorkbenchCustomizerWebPartStrings;
+  export = strings;
+}
