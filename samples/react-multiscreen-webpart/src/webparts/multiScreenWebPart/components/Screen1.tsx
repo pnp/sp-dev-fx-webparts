61d1832d3679caf2951450a0f85fc304aba3d0db
Velin Georgiev <velin.georgiev@gmail.com>
2019-07-03 15:52:08 +0100

    Adds react multi screen web part sample

---
+++ samples/react-multiscreen-webpart/src/webparts/multiScreenWebPart/components/Screen1.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export class Screen1 extends React.Component<{}, {}> {
+  public render(): React.ReactElement<{}> {
+    return (
+      <div>
+        <h2>Screen 1</h2>
+      </div>
+    );
+  }
+}
