3e82d22c5fe1e25b7afb1b921698a49a8003e174
joaojmendes <joao.j.mendes@outlook.com>
2019-07-05 13:57:15 +0100

    New Sample - react-multimedia-gallery - SPFx (#899)
    * update docs
    
    * Update README.md
    
    * Update README.md
    
    * Support to IE11
    
    * fine tunning, added Fade on Images
    
    * use CSS transitions
    
    * clean extra comments

---
+++ samples/react-multimedia-gallery/src/webparts/imageGallery/components/RenderImage/IRenderImageState.ts
@@ -0,0 +1,4 @@
+
+export interface IImageState {
+  onHover:boolean;
+}
---
+++ samples/react-multimedia-gallery/src/webparts/imageGallery/loc/en-us.js
@@ -0,0 +1,12 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Please select site and library to show in Image Gallery",
+    "BasicGroupName": "Properties",
+    "ListFieldLabel": "Library",
+    "SiteUrlFieldLabel": 'Site Url',
+    "WebPartConfigButtonLabel": "Configure",
+    "WebpartConfigDescription": "Please configure Image Library ",
+    "WebpartConfigIconText": "Configure your Image Gallery Web Part",
+    "TitleLabel": 'Web Part Title'
+  }
+});
