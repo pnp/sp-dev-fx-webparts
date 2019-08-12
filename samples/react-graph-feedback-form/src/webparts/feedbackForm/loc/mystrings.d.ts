5ef8f1cf32c986c1888ac59a97dae4e9d5f0a583
Sergei Zheleznov <sksdes@gmail.com>
2019-08-12 16:00:45 +0200

    Added sample react-graph-feedback-form

---
+++ samples/react-graph-feedback-form/src/webparts/feedbackForm/loc/mystrings.d.ts
@@ -0,0 +1,11 @@
+declare interface IFeedbackFormWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  TargetEmailFieldLabel: string;
+  SubjectFieldLabel: string;
+}
+
+declare module 'FeedbackFormWebPartStrings' {
+  const strings: IFeedbackFormWebPartStrings;
+  export = strings;
+}
