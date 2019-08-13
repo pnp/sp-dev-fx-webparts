b629640c1f55b9a1d637c4ec62245c8b9ad22893
João Mendes <joao.j.mendes@outlook.com>
2019-08-13 23:16:40 +0100

    New Web Part MyFlows

---
+++ samples/js-myflows/src/webparts/myFlows/MyFlowsWebPart.ts
@@ -0,0 +1,111 @@
+import { Version, Guid } from "@microsoft/sp-core-library";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from "@microsoft/sp-property-pane";
+import { escape } from "@microsoft/sp-lodash-subset";
+
+import styles from "./MyFlowsWebPart.module.scss";
+import * as strings from "MyFlowsWebPartStrings";
+import service from "./../../services/services";
+
+export interface IMyFlowsWebPartProps {
+  title: string;
+}
+
+export default class MyFlowsWebPart extends BaseClientSideWebPart<
+  IMyFlowsWebPartProps
+> {
+  private _msFlowSdk: any = null;
+  private _services: service = null;
+  private _guid =  Guid.newGuid();
+  public constructor(props: IMyFlowsWebPartProps) {
+    super();
+
+    // Initialize flow SDK
+    this._msFlowSdk = window["MsFlowSdk"];
+  }
+
+  public render(): void {
+    this.domElement.setAttribute("Id", `"${this._guid}"`);
+    this.domElement.setAttribute("class", `"${styles.sdk}"`);
+    this.domElement.innerHTML = `<div><label class=${styles.title}>${this.properties.title}</label></div>`;
+    this._services = new service(this.context);
+    this._services.getAccessToken().then((token: string) => {
+      const flowSDK = new this._msFlowSdk({
+        hostName: "https://flow.microsoft.com",
+        locale: this.context.pageContext.cultureInfo.currentCultureName
+      });
+      // Render Flow widget
+      let widget: any = flowSDK.renderWidget("flows", {
+        container: `"${this._guid}"`,
+        sdkVersion: "1.1",
+        enableOnBehalfOfTokens: true,
+        debugMode: false,
+        allowOptionalEvents: true,
+        flowsSettings: {
+          createFromBlankTemplateId: "05ed784f63df4ac7b8cbb465005d6068",
+          encodedFlowsFilter: "",
+          isMini: false,
+          enableBusinessProcessFlow: true
+        },
+        templatesSettings: {
+          defaultParams: '',
+          category: "PowerAppsButton",
+          destination: "new",
+          metadataSortProperty: "",
+          pageSize: 6,
+          searchTerm: "",
+          useServerSideProvisioning: false,
+          showGoBack: true,
+          enableWidgetCloseOnFlowSave: false,
+          showCreateFromBlank: false,
+          enableDietDesigner: false,
+          showHiddenTemplates: false,
+          allowCustomFlowName: false,
+          oneClickCategory: "",
+          dietCategory: ""
+        },
+        widgetStyleSettings: {
+          backgroundColor: "",
+          themeName: ""
+        }
+      });
+      // Register handler
+      widget.listen("GET_ACCESS_TOKEN", (requestParam, widgetDoneCallback) => {
+        widgetDoneCallback(null, { token: token });
+      });
+      // Register handler
+      widget.listen("WIDGET_READY", () => {
+        console.log("The flow widget is now ready.");
+      });
+    });
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse("1.0");
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField("title", {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
