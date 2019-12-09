2d9b61fc4dd3fb64aebcf9fa5381e6751128e9c4
Federico <16118699+fredupstair@users.noreply.github.com>
2019-12-09 09:06:36 +0000

    SPFX Tour sample WebPart - react-tour-pnpjs (#1082)
    * Initial commit
    
    * first commit
    
    * first release
    
    * rework
    
    * load fix and styling
    
    * fix webpart select
    
    * css ReactTourJS fix
    
    * css fix
    
    * added assets
    
    * Update README.md
    
    * fix webPartData undefined on Text Webpart

---
+++ samples/react-tour-pnpjs/src/webparts/tour/loc/en-us.js
@@ -0,0 +1,8 @@
+define([], function() {
+  return {
+    "PropertyPaneDescription": "Configure the tutorial to explain your users all features of the page",
+    "BasicGroupName": "Group Name",
+    "DescriptionFieldLabel": "Tour Description",
+    "ActionValueFieldLabel":"Button Label",
+  }
+});
