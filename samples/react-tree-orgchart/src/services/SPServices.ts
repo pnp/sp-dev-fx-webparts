27878aa71f3f82cbc7acd9a13f74e5854a02b4ab
Jake Stanger <jake.stanger@adm-computing.co.uk>
2020-01-28 14:00:26 +0000

    refactor(react-tree-orgchart): remove whitespace

--- samples/react-tree-orgchart/src/services/SPServices.ts
+++ samples/react-tree-orgchart/src/services/SPServices.ts
@@ -1,5 +1,5 @@
 import { WebPartContext } from "@microsoft/sp-webpart-base";
-import { sp } from '@pnp/sp';
+import { sp } from "@pnp/sp";
 
 export default class SPServices {
   constructor(private context: WebPartContext) {
@@ -8,8 +8,8 @@ export default class SPServices {
     });
   }
 
-  public async getUserProperties(user:string) {
-    let currentUserProperties:any = await sp.profiles.getPropertiesFor(user);
+  public async getUserProperties(user: string) {
+    let currentUserProperties: any = await sp.profiles.getPropertiesFor(user);
     console.log(currentUserProperties);
 
     return currentUserProperties;
@@ -19,12 +19,13 @@ export default class SPServices {
    * async GetUserProfileProperty
    * user:string
    */
-  public async getUserProfileProperty(user:string,property:string) {
-    let UserProperty:any = await sp.profiles.getUserProfilePropertyFor(user, property);
+  public async getUserProfileProperty(user: string, property: string) {
+    let UserProperty: any = await sp.profiles.getUserProfilePropertyFor(
+      user,
+      property
+    );
     console.log(UserProperty);
 
     return UserProperty;
   }
-
-   
 }
