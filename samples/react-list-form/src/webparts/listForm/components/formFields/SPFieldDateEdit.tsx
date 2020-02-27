6ed060f333c3da99d4a48dfab3147644b4c1d546
dudelis <fukszon.kostya@gmail.com>
2020-02-27 23:48:53 +0100

    #1 - Removed intermediary component, which was only sending data

--- samples/react-list-form/src/webparts/listForm/components/formFields/SPFieldDateEdit.tsx
+++ samples/react-list-form/src/webparts/listForm/components/formFields/SPFieldDateEdit.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import * as moment from 'moment';
 import { css } from 'office-ui-fabric-react/lib/Utilities';
+import { DatePicker, DayOfWeek, IDatePickerProps, IDatePickerStrings } from 'office-ui-fabric-react/lib/DatePicker';
+
 import { Locales } from '../../../../common/Locales';
 import { ISPFormFieldProps } from './SPFormField';
 import DateFormField from './DateFormField';
@@ -10,17 +12,22 @@ import styles from './SPFormField.module.scss';
 
 const SPFieldDateEdit: React.SFC<ISPFormFieldProps> = (props) => {
   const locale = Locales[props.fieldSchema.LocaleId];
-  return <DateFormField
-    {...props.value && moment(props.value).isValid() ? { value: moment(props.value).toDate() } : {}}
-    className={css(styles.dateFormField, 'ard-dateFormField')}
-    placeholder={strings.DateFormFieldPlaceholder}
-    isRequired={props.fieldSchema.Required}
-    ariaLabel={props.fieldSchema.Title}
-    locale={Locales[locale]}
-    firstDayOfWeek={props.fieldSchema.FirstDayOfWeek}
-    allowTextInput
-    onSelectDate={(date) => props.valueChanged(date.toLocaleDateString(locale))}
-  />;
+  return (
+    <React.Fragment>
+      <DatePicker
+        {...props.value && moment(props.value).isValid() ? { value: moment(props.value).toDate() } : {}}
+        allowTextInput={true}
+        ariaLabel={props.fieldSchema.Title}
+        className={css(styles.dateFormField, 'ard-dateFormField')}
+        firstDayOfWeek={props.fieldSchema.FirstDayOfWeek}
+        formatDate={(date: Date) => (typeof date.toLocaleDateString === 'function') ? date.toLocaleDateString(Locales[locale]) : ''}
+        isRequired={props.fieldSchema.Required}
+        onSelectDate={(date) => props.valueChanged(date.toLocaleDateString(Locales[locale]))}
+        parseDateFromString={(dateStr: string) => new Date(Date.parse(dateStr))}
+        placeholder={strings.DateFormFieldPlaceholder}
+        strings={strings}
+      />
+    </React.Fragment>)
 };
 
 export default SPFieldDateEdit;
