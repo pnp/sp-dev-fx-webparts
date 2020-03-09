c837006bf628da2cf19424a348eddc33f42b3472
Konstantin <kofukszo@microsoft.com>
2020-03-09 19:29:39 +0100

    Attachment for react list form (#1167)
    * #1 Add CSS classes for correct layout from Office UI Fabric
    
    * #1 - Removed intermediary component, which was only sending data
    
    * #1 - Add all the component methods, carried out some simple tests
    
    * #01 - Remove CSS styles from the FormField. Only date controls have those
    
    * #01 - Fix some minor issues
    
    * Add a lot about attachments
    
    * Fixed tha files with the same name cannot be added
    
    * Added attachments functionality
    
    * Fixed the bug with multiple attachments delete
    
    * Removed console.log lines
    
    Co-authored-by: SharePoint Student <student@CONTOSOLTD.LOCAL>

--- samples/react-list-form/src/common/services/ListFormService.ts
+++ samples/react-list-form/src/common/services/ListFormService.ts
@@ -4,6 +4,8 @@ import * as strings from 'servicesStrings';
 import { ControlMode } from '../datatypes/ControlMode';
 import { IFieldSchema, RenderListDataOptions } from './datatypes/RenderListData';
 import { IListFormService } from './IListFormService';
+import { IAttachment } from '../../types/IAttachment';
+
 
 export class ListFormService implements IListFormService {
 
@@ -118,41 +120,45 @@ export class ListFormService implements IListFormService {
      * @param originalData An object containing all the field values retrieved on loading from list item.
      * @returns Promise object represents the updated or erroneous form field values.
      */
-    public updateItem(webUrl: string, listUrl: string, itemId: number,
-        fieldsSchema: IFieldSchema[], data: any, originalData: any): Promise<any> {
-        return new Promise<any>((resolve, reject) => {
-            const httpClientOptions: ISPHttpClientOptions = {
-                headers: {
-                    'Accept': 'application/json;odata=verbose',
-                    'Content-type': 'application/json;odata=verbose',
-                    'X-SP-REQUESTRESOURCES': 'listUrl=' + encodeURIComponent(listUrl),
-                    'odata-version': '',
-                },
-            };
-            const formValues = this.GetFormValues(fieldsSchema, data, originalData);
-
-            httpClientOptions.body = JSON.stringify({
-                bNewDocumentUpdate: false,
-                checkInComment: null,
-                formValues,
-            });
-            const endpoint = `${webUrl}/_api/web/GetList(@listUrl)/items(@itemId)/ValidateUpdateListItem()`
-                + `?@listUrl=${encodeURIComponent('\'' + listUrl + '\'')}&@itemId=%27${itemId}%27`;
-            this.spHttpClient.post(endpoint, SPHttpClient.configurations.v1, httpClientOptions)
-                .then((response: SPHttpClientResponse) => {
-                    if (response.ok) {
-                        return response.json();
-                    } else {
-                        reject(this.getErrorMessage(webUrl, response));
-                    }
-                })
-                .then((respData) => {
-                    resolve(respData.d.ValidateUpdateListItem.results);
-                })
-                .catch((error) => {
-                    reject(this.getErrorMessage(webUrl, error));
-                });
+    public updateItem = async (webUrl: string, listUrl: string, itemId: number, fieldsSchema: IFieldSchema[], data: any, originalData: any) => {
+        const httpClientOptions: ISPHttpClientOptions = {
+            headers: {
+                'Accept': 'application/json;odata=verbose',
+                'Content-type': 'application/json;odata=verbose',
+                'X-SP-REQUESTRESOURCES': 'listUrl=' + encodeURIComponent(listUrl),
+                'odata-version': '',
+            },
+        };
+        const formValues = this.GetFormValues(fieldsSchema, data, originalData);
+        let createAttachmetns = this.GetAttachmentsCreate(data);
+        let deleteAttachmetns = this.GetAttachmentsDelete(data, originalData);
+        httpClientOptions.body = JSON.stringify({
+            bNewDocumentUpdate: false,
+            checkInComment: null,
+            formValues,
         });
+        const endpoint = `${webUrl}/_api/web/GetList(@listUrl)/items(@itemId)/ValidateUpdateListItem()`
+            + `?@listUrl=${encodeURIComponent('\'' + listUrl + '\'')}&@itemId=%27${itemId}%27`;
+        try {
+            let response = await this.spHttpClient.post(endpoint, SPHttpClient.configurations.v1, httpClientOptions);
+            if (!response.ok) {
+                return this.getErrorMessage(webUrl, response);
+            }
+            let responseData = await response.json();
+            responseData.AttachmentResponse = [];
+            if (deleteAttachmetns.length > 0) {
+                let deleteResponse = await this.deleteAttachments(webUrl, listUrl, itemId, deleteAttachmetns);
+                responseData.AttachmentResponse.push(deleteResponse);
+            }
+            if (createAttachmetns.length > 0) {
+                let createResponse = await this.uploadAttachments(webUrl, listUrl, itemId, createAttachmetns);
+                responseData.AttachmentResponse.push(createResponse);
+            }
+            return responseData.d.ValidateUpdateListItem.results;
+        } catch (error) {
+            return this.getErrorMessage(webUrl, error);
+        }
+
     }
 
     /**
@@ -167,6 +173,7 @@ export class ListFormService implements IListFormService {
     public createItem(webUrl: string, listUrl: string, fieldsSchema: IFieldSchema[], data: any): Promise<any> {
         return new Promise<any>((resolve, reject) => {
             const formValues = this.GetFormValues(fieldsSchema, data, {});
+            const formAttachmetns = this.GetAttachmentsCreate(data);
             const httpClientOptions: ISPHttpClientOptions = {
                 headers: {
                     'Accept': 'application/json;odata=verbose',
@@ -198,12 +205,79 @@ export class ListFormService implements IListFormService {
                     }
                 })
                 .then((respData) => {
+                    let itemId = respData.d.AddValidateUpdateItemUsingPath.results.find(item => {
+                        return item.FieldName == "Id";
+                    }).FieldValue;
+                    //if there are attachments, we upload all of them.
+                    if (formAttachmetns.length > 0) {
+                        this.uploadAttachments(webUrl, listUrl, itemId, formAttachmetns)
+                            .then((attachmentResponse) => {
+                                respData.AttachmentResponse = attachmentResponse;
+                                resolve(respData.d.AddValidateUpdateItemUsingPath.results);
+                            })
+                            .catch((error) => {
+                                reject(this.getErrorMessage(webUrl, error));
+                            });
+                    }
                     resolve(respData.d.AddValidateUpdateItemUsingPath.results);
                 })
                 .catch((error) => {
                     reject(this.getErrorMessage(webUrl, error));
                 });
         });
+    }
+
+    private uploadAttachments = async (webUrl: string, listUrl: string, itemId: number, attachments: any) => {
+        let responses = [];
+        for (var i = 0; i < attachments.length; i++) {
+            let attachment = attachments[i];
+            let httpClientOptions: ISPHttpClientOptions = {
+                headers: {
+                    "Accept": "application/json; odata=verbose",
+                    "content-type": "application/json; odata=verbose",
+                    "content-length": attachment.buffer.byteLength,
+                    'X-SP-REQUESTRESOURCES': 'listUrl=' + encodeURIComponent(listUrl),
+                    'odata-version': '',
+                },
+                body: attachment.buffer,
+            };
+            let endpoint = `${webUrl}/_api/web/GetList(@listUrl)/items(@itemId)/AttachmentFiles/add(FileName='${attachment.fileName}')`
+                + `?@listUrl=${encodeURIComponent('\'' + listUrl + '\'')}&@itemId=%27${itemId}%27`;
+            try {
+                let response = await this.spHttpClient.post(endpoint, SPHttpClient.configurations.v1, httpClientOptions);
+                if (response.ok) {
+                    let respJson = await response.json();
+                    responses.push(respJson);
+                } else {
+                    return this.getErrorMessage(webUrl, response);
+                }
+            } catch (error) {
+                return error;
+            }
+        }
+        return responses;
+    }
+    private deleteAttachments = async (webUrl: string, listUrl: string, itemId: number, attachments: string[]) => {
+        let responses = [];
+        for (const attachment of attachments) {
+            let httpClientOptions: ISPHttpClientOptions = {
+                headers: {
+                    "Accept": "application/json; odata=verbose",
+                    "content-type": "application/json; odata=verbose",
+                    'X-SP-REQUESTRESOURCES': 'listUrl=' + encodeURIComponent(listUrl),
+                    "X-HTTP-Method": "DELETE",
+                    'odata-version': '',
+                }
+            };
+            let endpoint = `${webUrl}/_api/web/GetList(@listUrl)/items(@itemId)/AttachmentFiles/getByFileName('${attachment}')`
+                + `?@listUrl=${encodeURIComponent('\'' + listUrl + '\'')}&@itemId=%27${itemId}%27`;
+            try {
+                await this.spHttpClient.post(endpoint, SPHttpClient.configurations.v1, httpClientOptions);
+            } catch (error) {
+                return error;
+            }
+        }
+        return responses;
     }
 
     private GetFormValues(fieldsSchema: IFieldSchema[], data: any, originalData: any)
@@ -214,8 +288,8 @@ export class ListFormService implements IListFormService {
                 && (field.InternalName in data)
                 && (data[field.InternalName] !== null)
                 && (data[field.InternalName] !== originalData[field.InternalName])
-            ),
-        )
+                && (field.InternalName != "Attachments")
+            ))
             .map((field) => {
                 return {
                     ErrorMessage: null,
@@ -223,8 +297,37 @@ export class ListFormService implements IListFormService {
                     FieldValue: data[field.InternalName],
                     HasException: false,
                 };
-            },
-            );
+            });
+    }
+    private GetAttachmentsCreate(data: any)
+        : Array<{ buffer: any, bufferLength: number, fileName: string }> {
+        var results = new Array<{ buffer: any, bufferLength: number, fileName: string }>();
+        if (data.Attachments && data.Attachments.length > 0) {
+            results = data.Attachments
+                .filter((attachment: IAttachment) => typeof attachment.AttachmentId == "undefined")
+                .map((attachment: IAttachment) => {
+                    return {
+                        buffer: attachment.FileBuffer,
+                        bufferLength: attachment.FileBuffer.byteLength,
+                        fileName: attachment.FileName
+                    } as { buffer: any, bufferLength: number, fileName: string };
+                });
+        }
+        return results;
+    }
+    private GetAttachmentsDelete(data: any, originalData: any)
+        : Array<string> {
+        let results = new Array<string>();
+
+        let newAttachments = (typeof data.Attachments.Attachments == "undefined") ? data.Attachments : data.Attachments.Attachments;
+        if (originalData && originalData.Attachments && originalData.Attachments.Attachments) {
+            let filtered = originalData.Attachments.Attachments
+                .filter((originalAttachment: IAttachment) =>
+                    !newAttachments.some((attachment: IAttachment) => attachment.FileName == originalAttachment.FileName)
+                );
+            results = filtered.map((originalAttachment: IAttachment) => originalAttachment.FileName);
+        }
+        return results;
     }
 
     /**
