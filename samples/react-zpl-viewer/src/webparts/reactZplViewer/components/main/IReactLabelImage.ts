3a7a3746ff307354100bd5bb6b0d2f9261861db2
Zach <zacharyroberts4411@gmail.com>
2020-03-09 13:57:48 -0400

    react-zpl-viewer sample (#1140)
    * Initial Commit
    
    * Fixed file select and updated ReadME
    
    * updated ReadME

---
+++ samples/react-zpl-viewer/src/webparts/reactZplViewer/components/main/IReactLabelImage.ts
@@ -0,0 +1,5 @@
+export interface labelImage {
+  url?: string;
+  width?: string;
+  height?: string;
+}
---
+++ samples/react-zpl-viewer/src/webparts/reactZplViewer/components/main/IReactZplViewerProps.ts
@@ -0,0 +1,7 @@
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+
+export interface IReactZplViewerProps {
+  description: string;
+  title: string;
+  context: WebPartContext;
+}
