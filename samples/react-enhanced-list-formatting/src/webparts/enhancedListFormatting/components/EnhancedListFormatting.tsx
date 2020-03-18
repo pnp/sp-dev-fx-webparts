c095eb074894c0f5fcce92aac118a295aa9af712
Hugo Bernier <hugoabernier@live.ca>
2020-03-18 18:03:49 -0400

    Fixed minor issue with read only render

--- samples/react-enhanced-list-formatting/src/webparts/enhancedListFormatting/components/EnhancedListFormatting.tsx
+++ samples/react-enhanced-list-formatting/src/webparts/enhancedListFormatting/components/EnhancedListFormatting.tsx
@@ -9,28 +9,7 @@ export default class EnhancedListFormatting extends React.Component<IEnhancedLis
   public render(): React.ReactElement<IEnhancedListFormattingProps> {
     const { css, displayMode, acceptedDisclaimer } = this.props;
 
-    // If we're not in Edit mode, hide this web part
-    if (displayMode !== DisplayMode.Edit) {
-      return null;
-    }
-
-    // if we didn't accept the disclaimer, show a disclaimer and nothing else
-    if (acceptedDisclaimer !== true) {
-      return (<MessageBar
-        onDismiss={()=>this._onAcceptDisclaimer()}
-        dismissButtonAriaLabel={strings.DismissDisclaimerAriaLabel}
-        messageBarType={MessageBarType.warning}
-        actions={
-          <div>
-            <MessageBarButton onClick={()=>this._onAcceptDisclaimer()}>{strings.AcceptDisclaimerButton}</MessageBarButton>
-          </div>
-        }
-      >
-        <div className={styles.disclaimerText} dangerouslySetInnerHTML={{__html: strings.DisclaimerText}}></div>
-      </MessageBar>);
-    }
-
-    // If we accepted the disclaimer, let's work as expected
+     // If we accepted the disclaimer, let's work as expected
 
     // Determine if there is a CSS value
     const hasCSS: boolean = css !== undefined && css !== "";
@@ -74,6 +53,29 @@ export default class EnhancedListFormatting extends React.Component<IEnhancedLis
         styleElem.textContent = "";
       }
     }
+
+    // If we're not in Edit mode, hide this web part
+    if (displayMode !== DisplayMode.Edit) {
+      return null;
+    }
+
+    // if we didn't accept the disclaimer, show a disclaimer and nothing else
+    if (acceptedDisclaimer !== true) {
+      return (<MessageBar
+        onDismiss={()=>this._onAcceptDisclaimer()}
+        dismissButtonAriaLabel={strings.DismissDisclaimerAriaLabel}
+        messageBarType={MessageBarType.warning}
+        actions={
+          <div>
+            <MessageBarButton onClick={()=>this._onAcceptDisclaimer()}>{strings.AcceptDisclaimerButton}</MessageBarButton>
+          </div>
+        }
+      >
+        <div className={styles.disclaimerText} dangerouslySetInnerHTML={{__html: strings.DisclaimerText}}></div>
+      </MessageBar>);
+    }
+
+
 
     return (
       <div className={styles.enhancedListFormatting}>
