8c6f2bb4ca1dd1bc4485c349f83dee29d3445c45
Joel Rodrigues <19577724+joelfmrodrigues@users.noreply.github.com>
2020-06-03 23:49:19 +0100

    Update web part description

--- samples/js-workbench-customizer/src/webparts/workbenchCustomizer/loc/en-us.js
+++ samples/js-workbench-customizer/src/webparts/workbenchCustomizer/loc/en-us.js
@@ -1,6 +1,6 @@
 define([], function() {
   return {
-    "PropertyPaneDescription": "Description",
+    "PropertyPaneDescription": "Web part that can customize the behaviour of the workbench page to work around some of the existing limitations. It's recommended that you refresh the page after changing web part properties.",
     "BasicGroupName": "Configuration",
     "CustomWorkbenchStylesFieldLabel": "Enable custom styles for Workbench",
     "customWorkbenchStylesFullWidthFieldLabel": "Enable custom styles for full-width",
