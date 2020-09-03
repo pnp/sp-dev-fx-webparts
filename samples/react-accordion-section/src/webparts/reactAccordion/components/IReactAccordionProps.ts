30f3c2ca8063eb99d0d89ec76c16019ff8afefd9
Hugo Bernier <hugoabernier@live.ca>
2020-09-03 00:50:03 -0400

    Merged Mike Zimmerman's changes

--- samples/react-accordion-section/src/webparts/reactAccordion/components/IReactAccordionProps.ts
+++ samples/react-accordion-section/src/webparts/reactAccordion/components/IReactAccordionProps.ts
@@ -1,5 +1,11 @@
+import { DisplayMode } from '@microsoft/sp-core-library';
+
 export interface IReactAccordionProps {
   listId: string;
   accordionTitle: string;
+  allowZeroExpanded: boolean;
+  allowMultipleExpanded: boolean;
+  displayMode: DisplayMode;
+  updateProperty: (value: string) => void;
   onConfigure: () => void;
 }
