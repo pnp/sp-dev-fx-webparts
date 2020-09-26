2c861fbe7860df95c6101b1cbf079f96bf7b55c4
Ryan Schouten <ryan@sharepointknight.com>
2020-09-26 12:12:39 -0700

    Fix date handling problems and redirect after edit

--- samples/react-list-form/src/webparts/listForm/ListFormWebPart.ts
+++ samples/react-list-form/src/webparts/listForm/ListFormWebPart.ts
@@ -34,6 +34,29 @@ export default class ListFormWebPart extends BaseClientSideWebPart<IListFormWebP
   protected onInit(): Promise<void> {
     return super.onInit().then((_) => {
       this.listService = new ListService(this.context.spHttpClient);
+      //Polyfill array find
+      if (!Array.prototype["find"]) {
+        Array.prototype["find"] = function (predicate) {
+          if (this == null) {
+            throw new TypeError('Array.prototype.find called on null or undefined');
+          }
+          if (typeof predicate !== 'function') {
+            throw new TypeError('predicate must be a function');
+          }
+          var list = Object(this);
+          var length = list.length >>> 0;
+          var thisArg = arguments[1];
+          var value;
+
+          for (var i = 0; i < length; i++) {
+            value = list[i];
+            if (predicate.call(thisArg, value, i, list)) {
+              return value;
+            }
+          }
+          return undefined;
+        };
+      }
     });
   }
 
