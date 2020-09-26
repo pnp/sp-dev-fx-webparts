6eb9677a74108f47bf8d157600cb5a889b96415b
Jerry Yasir <yasir.attiq@gmail.com>
2020-09-26 15:54:06 -0400

    first

---
+++ samples/react-world-clocks/lib/webparts/worldClockWebPart/components/WorldClockWebPart.module.scss.d.ts
@@ -0,0 +1,16 @@
+declare const styles: {
+    worldClockWebPart: string;
+    container: string;
+    Clock: string;
+    ClockTitle: string;
+    row: string;
+    column: string;
+    'ms-Grid': string;
+    title: string;
+    subTitle: string;
+    description: string;
+    button: string;
+    label: string;
+};
+export default styles;
+//# sourceMappingURL=WorldClockWebPart.module.scss.d.ts.map
---
+++ samples/react-world-clocks/lib/webparts/worldClockWebPart/components/WorldClockWebPart.module.scss.js
@@ -0,0 +1,19 @@
+/* tslint:disable */
+require("./WorldClockWebPart.module.css");
+var styles = {
+    worldClockWebPart: 'worldClockWebPart_5c7ebacf',
+    container: 'container_5c7ebacf',
+    Clock: 'Clock_5c7ebacf',
+    ClockTitle: 'ClockTitle_5c7ebacf',
+    row: 'row_5c7ebacf',
+    column: 'column_5c7ebacf',
+    'ms-Grid': 'ms-Grid_5c7ebacf',
+    title: 'title_5c7ebacf',
+    subTitle: 'subTitle_5c7ebacf',
+    description: 'description_5c7ebacf',
+    button: 'button_5c7ebacf',
+    label: 'label_5c7ebacf'
+};
+export default styles;
+/* tslint:enable */ 
+//# sourceMappingURL=WorldClockWebPart.module.scss.js.map
