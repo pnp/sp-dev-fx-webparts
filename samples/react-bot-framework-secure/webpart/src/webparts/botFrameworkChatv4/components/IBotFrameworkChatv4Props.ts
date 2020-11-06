f1de950d46fe771bc3adcb2b663b68f6fa7adb58
Qiong Wu <qiowu@microsoft.com>
2020-11-06 17:27:45 +0800

    Revert "v1.0"
    This reverts commit 9f2ca3ddb6d284ba00cb35d8977174f25983c7b2.

--- samples/react-bot-framework-secure/webpart/src/webparts/botFrameworkChatv4/components/IBotFrameworkChatv4Props.ts
+++
@@ -1,7 +0,0 @@
-import { WebPartContext } from '@microsoft/sp-webpart-base';
-
-export interface IBotFrameworkChatv4Props {
-  botEndpoint: string;
-  botName: string;
-  context: WebPartContext;
-}
--- samples/react-bot-framework-secure/webpart/src/webparts/botFrameworkChatv4/loc/en-us.js
+++
@@ -1,8 +0,0 @@
-define([], function() {
-  return {
-    "PropertyPaneDescription": "Set up bot configuration.",
-    "BasicGroupName": "Config",
-    "BotEndpointLabel": "Bot Endpoint (For example: https://YOUR_BOT.azurewebsites.net)",
-    "BotName": "Bot"
-  }
-});
