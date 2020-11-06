a1283433313ee165aae07655ff841d6ad66420a6
Don Kirkham <donkirkham@live.com>
2020-11-06 16:35:09 -0600

    new react-SectionTheme webpart

--- samples/react-themes/src/webparts/sectionTheme/components/SectionTheme.tsx
+++ samples/react-themes/src/webparts/sectionTheme/components/SectionTheme.tsx
@@ -1,18 +1,10 @@
 import * as React from 'react';
 import styles from './SectionTheme.module.scss';
 import { ISectionThemeProps } from './ISectionThemeProps';
-<<<<<<< HEAD
-import { ISemanticColors } from '@microsoft/sp-component-base';
-
-export default class SectionTheme extends React.Component<ISectionThemeProps, {}> {
-
-  // Build array of semantic colors values to display in webparts.
-=======
 import { IReadonlyTheme } from '@microsoft/sp-component-base';
 
 export default class SectionTheme extends React.Component<ISectionThemeProps, {}> {
   //private _styles: object[];
->>>>>>> new react-SectionTheme webpart
   private getSemanticValues(theming: any): { key: string, value: string }[] {
     if (!theming) { return null; }
     let _styles: { key: string, value: string }[] = [];
@@ -26,35 +18,13 @@ export default class SectionTheme extends React.Component<ISectionThemeProps, {}
   }
 
   public render(): React.ReactElement<ISectionThemeProps> {
-<<<<<<< HEAD
-    const semanticColors : ISemanticColors = this.props.themeVariant.semanticColors;
-    return (
-=======
     const { semanticColors }: IReadonlyTheme = this.props.themeVariant;
     return (
       // <div className={styles.sectionTheme} style={{ backgroundColor: semanticColors.bodyBackground, color: semanticColors.bodyText }} data-load-themed-styles="true" >
->>>>>>> new react-SectionTheme webpart
       <div className={styles.sectionTheme} data-load-themed-styles="true" >
         <div className={styles.container} >
           <div className={styles.row}>
             <div className={styles.column}>
-<<<<<<< HEAD
-              <p className={styles.title}>Change site theme and section background color</p>
-              <p className={styles.subTitle}>Current theme variant (section values)</p>
-              <div className={styles.details}>
-                {
-                  this.getSemanticValues(semanticColors).map((item) => (
-                    <div style={{ lineHeight: "2em" }}>
-                      <div className={styles.itemKey} >{item.key}:</div>
-                      <div className={styles.itemValue}>
-                        <span className={styles.colorBlock} style={{ background: item.value }}>______</span>
-                        {item.value}
-                      </div>
-                    </div>
-                  ))
-                }
-              </div>
-=======
               <p className={styles.title}>Custom styling based on section</p>
               <span className={styles.subTitle}>Current theme variant (section values)</span>
               <ul>
@@ -63,15 +33,10 @@ export default class SectionTheme extends React.Component<ISectionThemeProps, {}
                 })
                 }
               </ul>
->>>>>>> new react-SectionTheme webpart
             </div>
           </div>
         </div>
       </div>
     );
   }
-<<<<<<< HEAD
-
-=======
->>>>>>> new react-SectionTheme webpart
 }
