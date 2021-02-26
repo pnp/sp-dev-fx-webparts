0d5698dcaf88faf0c2e921f1b59b873a077d8c56
Peter Paul Kirschner <peter@kirschner.zone>
2021-02-26 23:05:40 +0100

    adding Service

---
+++ samples/react-tree-orgchart/src/services/GraphService.ts
@@ -0,0 +1,36 @@
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+import { graph } from "@pnp/graph";
+
+export interface IGraphUser {
+    mail?: string;
+    displayName?: string;
+    jobTitle?: string;
+}
+
+const graphUserSelect: string[] = ['displayName', 'mail', 'jobTitle'];
+
+export default class GraphServices {
+
+
+    constructor(private context: WebPartContext) {
+        graph.setup({
+            spfxContext: this.context
+        });
+    }
+
+    public async getUser(upn: string): Promise<IGraphUser> {
+
+        return await graph.users.getById(upn).select(...graphUserSelect).get() as IGraphUser;
+    }
+
+    public async getUserManger(upn: string): Promise<IGraphUser> {
+
+        return await graph.users.getById(upn).manager.select(...graphUserSelect).get() as IGraphUser;
+    }
+    public async getUserDirectReports(upn: string) {
+
+        return await graph.users.getById(upn).directReports.select(...graphUserSelect).get() as IGraphUser[];
+    }
+
+}
+
