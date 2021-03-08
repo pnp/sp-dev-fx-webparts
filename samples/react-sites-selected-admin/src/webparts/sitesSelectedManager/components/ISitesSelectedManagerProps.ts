a62ac9e0e06da0eeaf98fab46d920acf7d57dbed
Fredrik Thorild <fredrik.thorild@sogeti.com>
2021-03-08 18:49:33 +0100

    Switch to functional components and re-factor
    Code clean up, no chnages to the UI.

--- samples/react-sites-selected-admin/src/webparts/sitesSelectedManager/components/ISitesSelectedManagerProps.ts
+++
@@ -1,8 +0,0 @@
-import { WebPartContext } from '@microsoft/sp-webpart-base';
-
-export interface ISitesSelectedManagerProps {
-  description: string;
-  context: WebPartContext;
-  showAbout: boolean;
-  aadGuid: string;
-}
