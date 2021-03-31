52f3df59075039a1ccf7eee687b7feb2e73d4e33
luismanez <luis.manez@outlook.com>
2021-03-31 11:38:47 +0200

    react-company-stories sample webpart added

---
+++ samples/react-company-stories/src/webparts/sharePointStories/SharePointStoriesWebPart.ts
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-property-pane';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+
+import * as strings from 'SharePointStoriesWebPartStrings';
+import SharePointStories from './components/SharePointStories';
+import { ISharePointStoriesProps } from './components/ISharePointStoriesProps';
+
+import { Providers, SharePointProvider } from '@microsoft/mgt';
+
+export interface ISharePointStoriesWebPartProps {
+  description: string;
+}
+
+export default class SharePointStoriesWebPart extends BaseClientSideWebPart<ISharePointStoriesWebPartProps> {
+
+  protected async onInit(): Promise<void> {
+    Providers.globalProvider = new SharePointProvider(this.context);
+  }
+
+  public render(): void {
+    const element: React.ReactElement<ISharePointStoriesProps> = React.createElement(
+      SharePointStories,
+      {
+        webpartContext: this.context
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
