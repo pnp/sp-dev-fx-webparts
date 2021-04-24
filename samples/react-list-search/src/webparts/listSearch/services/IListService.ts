72d3f3159560271eac0b34e334bf1b2c8dbb3db1
albegut <albegut@users.noreply.github.com>
2021-04-25 00:14:08 +0200

    Audience Support and Look&Feel changes

--- samples/react-list-search/src/webparts/listSearch/services/IListService.ts
+++ samples/react-list-search/src/webparts/listSearch/services/IListService.ts
@@ -1,8 +1,9 @@
 import { ListField } from "../model/IListConfigProps";
 import { IListSearchListQuery } from "../model/IMapQuery";
+import GraphService from "./GraphService";
 
 export default interface IListService {
-  getListItems(listQueryOptions: IListSearchListQuery, listPropertyName: string, sitePropertyName: string, sitePropertyValue: string, rowLimit: number): Promise<Array<any>>;
+  getListItems(listQueryOptions: IListSearchListQuery, listPropertyName: string, sitePropertyName: string, sitePropertyValue: string, rowLimit: number, graphService?: GraphService): Promise<Array<any>>;
   getListItemById(listQueryOptions: IListSearchListQuery, itemId: number): Promise<any>;
   getSiteListsTitle(): Promise<Array<any>>;
   getListFields(listTitle: string): Promise<Array<ListField>>;
