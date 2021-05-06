54d0c6ecb326d955444e4ab4e27d99e6d51403c6
ReactIntern <64291174+ReactIntern@users.noreply.github.com>
2021-05-06 00:20:20 -0400

    initial pr for react-groups-teams
    initial pull request for react groups & teams web part

---
+++ samples/react-groups-teams/src/webparts/microsoftGroups/MicrosoftGroupsWebPart.ts
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'MicrosoftGroupsWebPartStrings';
+import MicrosoftGroups from './components/MicrosoftGroups';
+
+export interface IMicrosoftGroupsWebPartProps {
+  description: string;
+}
+
+export default class MicrosoftGroupsWebPart extends BaseClientSideWebPart<IMicrosoftGroupsWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IMicrosoftGroupsWebPartProps > = React.createElement(
+      MicrosoftGroups,
+      {
+        context: this.context
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
