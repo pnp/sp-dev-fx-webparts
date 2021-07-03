b7bd4932f2a13b61e93fa29bb302b06ced36ac32
Russell gove <russellgove@gmail.com>
2021-07-03 09:49:07 -0400

    Update AddLists.ps1
    fix sitescript

--- samples/react-private-libraries/scripts/AddLists.ps1
+++ samples/react-private-libraries/scripts/AddLists.ps1
@@ -1,5 +1,16 @@
 cd C:\Users\trwg1\sp-dev-fx-webparts\samples\react-private-libraries\scripts\
-#connect-pnponline -Url "https://russellwgove.sharepoint.com/sites/deleteme"
+connect-pnponline -Url "https://{tenant}.sharepoint.com/sites/{site}"
 $it=Get-Content '.\rfx.json'-Raw 
-Add-PNPSiteScript  -Title "RFx Lists" -Content $it
+$script=Add-PNPSiteScript  -Title "RFx Lists" -Content $it
 
+$design=Add-PNPSiteDesign `
+  -Title "Rfx Listes" `
+  -WebTemplate "64" `
+  -SiteScriptIds $script.Id `
+  -Description "Creates RFX Libraries"
+
+Invoke-PnPSiteDesign -Identity $design.Id
+
+Remove-PnPSiteDesign -Identity $design.Id  
+
+Remove-PnPSiteScript -Identity $script.Id
