4f239c5e5741730258f7c91142e52253a2dffef9
Ari Gunawan <ari.gunawan.3023@gmail.com>
2021-09-26 22:06:30 +0700

    Remove accidentally commit files

--- samples/react-react-query/src/webparts/reactQueryExample/components/DisplayItems/IDisplayItemsProps.ts
+++
@@ -1,6 +0,0 @@
-export interface IDisplayItemsProps {
-  listId: string;
-  isNeedRefreshData: boolean;
-  updateIsNeedRefreshData: (value: boolean) => void;
-  setSelectedItem: (selectedItem: any) => void;
-}
--- samples/react-react-query/src/webparts/reactQueryExample/components/Form/IFormProps.ts
+++
@@ -1,6 +0,0 @@
-export interface IFormProps {
-  listId: string;
-  updateIsNeedRefreshData: (value: boolean) => void;
-  selectedItem: any;
-  setSelectedItem: (selectedItem: any) => void;
-}
