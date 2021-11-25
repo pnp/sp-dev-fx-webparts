a0c73393c804bf9c3f202a0a61096b4503d0eb40
André Lage <ponto_portuca@hotmail.com>
2021-11-25 18:17:13 +0100

    updated to use Microsoft Graph follow

--- samples/react-follow-document/src/webparts/followDocumentWebPart/components/followDocumentPreview/IfollowDocumentPreviewProps.ts
+++ samples/react-follow-document/src/webparts/followDocumentWebPart/components/followDocumentPreview/IfollowDocumentPreviewProps.ts
@@ -1,9 +1,11 @@
 import { WebPartContext } from "@microsoft/sp-webpart-base";
+import { FollowDocument } from "../../models/followDocument";
 export interface IfollowDocumentPreviewProps {
   isOpen: boolean;
   url?:string;
   filename?:string;
   context: WebPartContext;
   visible?:boolean;
+  FollowDocument: FollowDocument;
 
 }
