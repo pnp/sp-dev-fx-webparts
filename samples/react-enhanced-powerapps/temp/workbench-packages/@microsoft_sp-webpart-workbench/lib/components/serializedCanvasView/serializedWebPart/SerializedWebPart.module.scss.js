ded268a1de11550bac294ac2415b50a7ebe15379
kunj-sangani <sangani.kunj@gmail.com>
2022-01-20 01:53:32 +0530

    Updated to SPFx v1.13.1

--- samples/react-enhanced-powerapps/temp/workbench-packages/@microsoft_sp-webpart-workbench/lib/components/serializedCanvasView/serializedWebPart/SerializedWebPart.module.scss.js
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-require("./SerializedWebPart.module.css");
-var styles = {
-    serializedWebPartItem: 'serializedWebPartItem_0c56850c'
-};
-export default styles;
-/* tslint:enable */ 
-//# sourceMappingURL=SerializedWebPart.module.scss.js.map
