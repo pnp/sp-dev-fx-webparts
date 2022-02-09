120045d2876ddf4c69df0c76621ac26e2ae66070
Hugo Bernier <hugoabernier@live.ca>
2022-02-09 00:31:49 -0500

    Merged #2215 manually

--- samples/react-enhanced-powerapps/gulpfile.js
+++ samples/react-enhanced-powerapps/gulpfile.js
@@ -1,83 +1,16 @@
 'use strict';
 
-// check if gulp dist was called
-if (process.argv.indexOf('dist') !== -1) {
-    // add ship options to command call
-    process.argv.push('--ship');
-}
-
-const path = require('path');
 const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
-const gulpSequence = require('gulp-sequence');
-
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
-// Create clean distrubution package
-gulp.task('dist', gulpSequence('clean', 'bundle', 'package-solution'));
-// Create clean development package
-gulp.task('dev', gulpSequence('clean', 'bundle', 'package-solution'));
+var getTasks = build.rig.getTasks;
+build.rig.getTasks = function () {
+  var result = getTasks.call(build.rig);
 
+  result.set('serve', result.get('serve-deprecated'));
 
-
-/**
- * Webpack Bundle Anlayzer
- * Reference and gulp task
- */
-if (process.argv.indexOf('--analyze') !== -1 ||
-  process.argv.indexOf('dist') !== -1 ||
-  process.argv.indexOf('dev') !== -1) {
-
-  const bundleAnalyzer = require('webpack-bundle-analyzer');
-
-  build.configureWebpack.mergeConfig({
-
-    additionalConfiguration: (generatedConfiguration) => {
-      const lastDirName = path.basename(__dirname);
-      const dropPath = path.join(__dirname, 'temp', 'stats');
-      generatedConfiguration.plugins.push(new bundleAnalyzer.BundleAnalyzerPlugin({
-        openAnalyzer: false,
-        analyzerMode: 'static',
-        reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
-        generateStatsFile: true,
-        statsFilename: path.join(dropPath, `${lastDirName}.stats.json`),
-        logLevel: 'error'
-      }));
-
-      return generatedConfiguration;
-    }
-
-  });
-}
-
-
-/**
- * StyleLinter configuration
- * Reference and custom gulp task
- */
-const stylelint = require('gulp-stylelint');
-
-/* Stylelinter sub task */
-let styleLintSubTask = build.subTask('stylelint', (gulp) => {
-
-
-    return gulp
-        .src('src/**/*.scss')
-        .pipe(stylelint({
-            failAfterError: false,
-            reporters: [{
-                formatter: 'string',
-                console: true
-            }]
-        }));
-});
-/* end sub task */
-
-build.rig.addPreBuildTask(styleLintSubTask);
-
-/**
- * Custom Framework Specific gulp tasks
- */
-
+  return result;
+};
 
 build.initialize(gulp);
