9d8d4e7655dada1cd55f48a7f2d7448d3f2d4134
Nick Brown <BrownN12@cardiff.ac.uk>
2022-04-20 17:35:00 +0100

    Added Fluent UI 9 demo Fluent UI 9 is the converged fluent ui project, due to go v1 in May 2022

---
+++ samples/react-fluentui-9/README.md
@@ -0,0 +1,58 @@
+# react-fluentui-9
+
+## Summary
+
+This demo's the use of the new [Fluent UI version 9](https://github.com/microsoft/fluentui/tree/master/packages/react-components) controls and theme provider.  1 UI framework which has native theming for both Teams, SharePoint and Windows
+
+![SharePoint View](assets/FQzNLB4XwAIFMRh.jpg "SharePoint View") 
+![Teams View](assets/FQzO9YjWUAgFlrU.jpg "Teams View")
+
+## Used SharePoint Framework Version
+
+![version](https://img.shields.io/badge/version-1.14-green.svg)
+
+## Applies to
+
+- [SharePoint Framework](https://aka.ms/spfx)
+- [Microsoft 365 tenant](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/set-up-your-developer-tenant)
+
+> Get your own free development tenant by subscribing to [Microsoft 365 developer program](http://aka.ms/o365devprogram)
+
+## Solution
+
+Solution|Author(s)
+--------|---------
+react-fluentui-9 | Nick Brown ([@techienickb](https://twitter.com/techienickb) / Jisc)
+
+## Version history
+
+Version|Date|Comments
+-------|----|--------
+1.0|April 20, 2022|Initial release
+
+## Disclaimer
+
+**THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.**
+
+---
+
+## Minimal Path to Awesome
+
+- Clone this repository
+- Ensure that you are at the solution folder
+- in the command-line run:
+  - **npm install**
+  - **gulp serve --nobrowser**
+
+## Features
+
+Very simple demo, the handling of the theme provider is interesting implmenting it and handling the custom themes sharepoint can use.
+
+## References
+
+- [Getting started with SharePoint Framework](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/set-up-your-developer-tenant)
+- [Building for Microsoft teams](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/build-for-teams-overview)
+- [Use Microsoft Graph in your solution](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/web-parts/get-started/using-microsoft-graph-apis)
+- [Publish SharePoint Framework applications to the Marketplace](https://docs.microsoft.com/en-us/sharepoint/dev/spfx/publish-to-marketplace-overview)
+- [Microsoft 365 Patterns and Practices](https://aka.ms/m365pnp) - Guidance, tooling, samples and open-source controls for your Microsoft 365 development
+- [Fluent UI version 9](https://github.com/microsoft/fluentui/tree/master/packages/react-components) - Converged Fluent UI components
---
+++ samples/react-fluentui-9/src/webparts/fluentUi9Demo/components/FluentUi9Demo.tsx
@@ -0,0 +1,192 @@
+import * as React from 'react';
+import styles from './FluentUi9Demo.module.scss';
+import { IFluentUi9DemoProps } from './IFluentUi9DemoProps';
+import { escape } from '@microsoft/sp-lodash-subset';
+import { Button, Link, Text, Caption, Body, Avatar, useId, ToggleButton, Slider, Menu, MenuTrigger, MenuButtonProps, MenuPopover, MenuList, MenuItem, SplitButton, Divider } from '@fluentui/react-components';
+import { Card, Tab, TabList, CardHeader, CardPreview, CardFooter, Label, Input, Checkbox, RadioGroup, Radio, Switch } from '@fluentui/react-components/unstable';
+import { ArrowReplyRegular, ShareRegular, DocumentText24Regular } from '@fluentui/react-icons';
+
+export default function FluentUi9Demo(props: IFluentUi9DemoProps) {
+  const { isDarkTheme, environmentMessage, hasTeamsContext, userDisplayName } = props;
+  const [tab, setTab] = React.useState<string | unknown>("buttons");
+  const [me, setMe] = React.useState<string|undefined>();
+  const outlineId = useId('input-outline');
+  const underlineId = useId('input-underline');
+
+  React.useEffect(() => {
+    props.context.msGraphClientFactory.getClient().then(async (client) => {
+      await client
+          .api('/me/photo/$value')
+          .responseType("blob")
+          .get()
+          .then((blob: Blob) : Promise<any> => {
+            return new Promise(resolve => {
+              const url = URL.createObjectURL(blob);
+              setMe(url);
+              resolve(url);
+            });
+          });
+        });
+  }, []);
+
+  return (
+    <section className={`${styles.fluentUi9Demo} ${hasTeamsContext ? styles.teams : ''}`}>
+      <div className={styles.welcome}>
+        <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
+        <h2>Well done, {escape(userDisplayName)}!</h2>
+        <div>{environmentMessage}</div>
+      </div>
+      <div>
+        <h3>Welcome to SharePoint Framework!</h3>
+        <p>
+          This demo's the use of Fluent UI 9 instead of using FabircUI.  The new framework combines Northstar and FlientUI.  Below are some demo's and they should render in Teams style when inside Teams.
+        </p>
+        <Text as="h3">Demo components</Text>
+        <div className={styles.stack}>
+          <TabList onTabSelect={(e, data) => setTab(data.value)} defaultSelectedValue="buttons">
+            <Tab value="buttons">Buttons</Tab>
+            <Tab value="cards">Cards</Tab>
+            <Tab value="inputs">Inputs</Tab>
+          </TabList>
+          {tab === 'buttons' && <div className={styles.stack} style={{padding: 10}}>
+            <Divider appearance='brand'>Button</Divider>
+            <div className={`${styles.stackHoz} ${styles.spaceBetween}`}>
+              <Button>Default</Button><Button appearance="primary">Primary</Button><Button appearance="outline">Outline</Button><Button appearance="subtle">Subtle</Button><Button appearance="transparent">Transparent</Button>
+            </div>
+            <Divider appearance='brand'>Toggle Button</Divider>
+            <div className={`${styles.stackHoz} ${styles.spaceBetween}`}>
+              <ToggleButton>Default</ToggleButton>
+              <ToggleButton appearance="primary">Primary</ToggleButton>
+              <ToggleButton appearance="outline">Outline</ToggleButton>
+              <ToggleButton appearance="subtle">Subtle</ToggleButton>
+              <ToggleButton appearance="transparent">Transparent</ToggleButton>
+            </div>
+            <Divider appearance='brand'>Split Button</Divider>
+            <div className={`${styles.stackHoz} ${styles.spaceBetween}`}>
+              <Menu positioning="below-end">
+                <MenuTrigger>
+                  {(triggerProps: MenuButtonProps) => <SplitButton menuButton={triggerProps}>Default</SplitButton>}
+                </MenuTrigger>
+
+                <MenuPopover>
+                  <MenuList>
+                    <MenuItem>Item a</MenuItem>
+                    <MenuItem>Item b</MenuItem>
+                  </MenuList>
+                </MenuPopover>
+              </Menu>
+
+              <Menu positioning="below-end">
+                <MenuTrigger>
+                  {(triggerProps: MenuButtonProps) => (
+                    <SplitButton menuButton={triggerProps} appearance="primary">
+                      Primary
+                    </SplitButton>
+                  )}
+                </MenuTrigger>
+
+                <MenuPopover>
+                  <MenuList>
+                    <MenuItem>Item a</MenuItem>
+                    <MenuItem>Item b</MenuItem>
+                  </MenuList>
+                </MenuPopover>
+              </Menu>
+
+              <Menu positioning="below-end">
+                <MenuTrigger>
+                  {(triggerProps: MenuButtonProps) => (
+                    <SplitButton menuButton={triggerProps} appearance="outline">
+                      Outline
+                    </SplitButton>
+                  )}
+                </MenuTrigger>
+
+                <MenuPopover>
+                  <MenuList>
+                    <MenuItem>Item a</MenuItem>
+                    <MenuItem>Item b</MenuItem>
+                  </MenuList>
+                </MenuPopover>
+              </Menu>
+
+              <Menu positioning="below-end">
+                <MenuTrigger>
+                  {(triggerProps: MenuButtonProps) => (
+                    <SplitButton menuButton={triggerProps} appearance="subtle">
+                      Subtle
+                    </SplitButton>
+                  )}
+                </MenuTrigger>
+
+                <MenuPopover>
+                  <MenuList>
+                    <MenuItem>Item a</MenuItem>
+                    <MenuItem>Item b</MenuItem>
+                  </MenuList>
+                </MenuPopover>
+              </Menu>
+
+              <Menu positioning="below-end">
+                <MenuTrigger>
+                  {(triggerProps: MenuButtonProps) => (
+                    <SplitButton menuButton={triggerProps} appearance="transparent">
+                      Transparent
+                    </SplitButton>
+                  )}
+                </MenuTrigger>
+
+                <MenuPopover>
+                  <MenuList>
+                    <MenuItem>Item a</MenuItem>
+                    <MenuItem>Item b</MenuItem>
+                  </MenuList>
+                </MenuPopover>
+              </Menu>
+            </div>
+          </div>}
+          {tab === 'cards' && <>
+            <Card>
+              <CardHeader
+                image={<Avatar name={userDisplayName} image={me ? {src: me } : null} />}
+                header={
+                  <Body>
+                    <b>{userDisplayName}</b> mentioned
+                  </Body>
+                }
+                description={<Caption>5h ago · About us - Overview</Caption>}
+              />
+
+              <CardPreview logo={<DocumentText24Regular />}>
+                <img src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} alt="Preview of a Word document " />
+              </CardPreview>
+              <CardFooter>
+                <Button icon={<ArrowReplyRegular fontSize={16} />}>Reply</Button>
+                <Button icon={<ShareRegular fontSize={16} />}>Share</Button>
+              </CardFooter>
+            </Card>
+          </>}
+          {tab === 'inputs' && <div style={{padding: 10}} className={styles.stack}>
+            <div><Label htmlFor={outlineId}>Outline (default)</Label><Input appearance="outline" id={outlineId} /></div>
+            <div><Label htmlFor={underlineId}>Underline</Label><Input appearance="underline" id={underlineId} /></div>
+            <Checkbox label="Option 1" /><Checkbox label="Option 2" checked /><Checkbox label="Option 3" checked="mixed" />
+            <RadioGroup layout="horizontal"><Radio value="A" label="Option A" /><Radio value="B" label="Option B" /><Radio value="C" label="Option C" /><Radio value="D" label="Option D" /></RadioGroup>
+            <Switch />
+            <Slider defaultValue={20} />
+          </div>}
+        </div>
+        <h4>Learn more about SPFx development:</h4>
+        <ul className={styles.links}>
+          <li><Link href="https://aka.ms/spfx" target="_blank">SharePoint Framework Overview</Link></li>
+          <li><Link href="https://aka.ms/spfx-yeoman-graph" target="_blank">Use Microsoft Graph in your solution</Link></li>
+          <li><Link href="https://aka.ms/spfx-yeoman-teams" target="_blank">Build for Microsoft Teams using SharePoint Framework</Link></li>
+          <li><Link href="https://aka.ms/spfx-yeoman-viva" target="_blank">Build for Microsoft Viva Connections using SharePoint Framework</Link></li>
+          <li><Link href="https://aka.ms/spfx-yeoman-store" target="_blank">Publish SharePoint Framework applications to the marketplace</Link></li>
+          <li><Link href="https://aka.ms/spfx-yeoman-api" target="_blank">SharePoint Framework API reference</Link></li>
+          <li><Link href="https://aka.ms/m365pnp" target="_blank">Microsoft 365 Developer Community</Link></li>
+          <li><Link href="https://aka.ms/fluentui-storybook" target="_blank">Microsoft Fluent UI v9</Link></li>
+        </ul>
+      </div>
+    </section>
+  );
+}
