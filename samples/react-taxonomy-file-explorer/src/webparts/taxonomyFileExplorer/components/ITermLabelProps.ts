91b09cb163103e67592eb6b26f54d5c4d1cc6402
mmsharepoint <markus.moeller.ed@outlook.de>
2022-07-19 11:32:40 +0200

    New feature Expand/Collapse all upgrd SPFx 1.15.0

--- samples/react-taxonomy-file-explorer/src/webparts/taxonomyFileExplorer/components/ITermLabelProps.ts
+++ samples/react-taxonomy-file-explorer/src/webparts/taxonomyFileExplorer/components/ITermLabelProps.ts
@@ -4,9 +4,12 @@ import { ITermNode } from "../../../model/ITermNode";
 export interface ITermLabelProps {
     node: ITermNode;
     selectedNode: string;
+    collapseAll: boolean;
+    expandAll: boolean;
     renderFiles: (files: IFileItem[]) => void;
     resetChecked: (s: string) => void;
     addTerm: (file: IFileItem, newValue: string) => void;
     replaceTerm: (file: IFileItem, newValue: string) => void;
     copyFile: (file: IFileItem, newValue: string) => void;
+    uploadFile: (file: any, newValue: string) => void;
 }
