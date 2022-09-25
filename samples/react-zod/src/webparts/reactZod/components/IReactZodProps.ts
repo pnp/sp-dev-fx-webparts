700e8b5ee847943359dda7f7ebc6bce8e614b824
AriGunawan <ari.gunawan.3023@gmail.com>
2022-09-25 21:26:40 +1000

    Add initial react zod

---
+++ samples/react-zod/src/webparts/reactZod/components/IReactZodProps.ts
@@ -0,0 +1,7 @@
+export interface IReactZodProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ samples/react-zod/src/webparts/reactZod/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IReactZodWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ReactZodWebPartStrings' {
+  const strings: IReactZodWebPartStrings;
+  export = strings;
+}
