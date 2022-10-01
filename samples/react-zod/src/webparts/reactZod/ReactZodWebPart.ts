4df690d03c77795847be5fcf4c8dfaedef56715e
AriGunawan <ari.gunawan.3023@gmail.com>
2022-10-01 19:47:54 +1000

    Add pnpjs

--- samples/react-zod/src/webparts/reactZod/ReactZodWebPart.ts
+++ samples/react-zod/src/webparts/reactZod/ReactZodWebPart.ts
@@ -1,24 +1,26 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import * as strings from "ReactZodWebPartStrings";
+
+import { Version } from "@microsoft/sp-core-library";
 import {
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-property-pane';
-import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
+  PropertyPaneTextField,
+} from "@microsoft/sp-property-pane";
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import { SPFI, spfi, SPFx } from "@pnp/sp";
 
-import * as strings from 'ReactZodWebPartStrings';
-import ReactZod from './components/ReactZod';
-import { IReactZodProps } from './components/IReactZodProps';
+import { IReactZodProps } from "./components/IReactZodProps";
+import ReactZod from "./components/ReactZod";
 
 export interface IReactZodWebPartProps {
   description: string;
 }
 
 export default class ReactZodWebPart extends BaseClientSideWebPart<IReactZodWebPartProps> {
-
   private _isDarkTheme: boolean = false;
-  private _environmentMessage: string = '';
+  private _environmentMessage: string = "";
+  private _spfi: SPFI;
 
   public render(): void {
     const element: React.ReactElement<IReactZodProps> = React.createElement(
@@ -28,25 +30,31 @@ export default class ReactZodWebPart extends BaseClientSideWebPart<IReactZodWebP
         isDarkTheme: this._isDarkTheme,
         environmentMessage: this._environmentMessage,
         hasTeamsContext: !!this.context.sdks.microsoftTeams,
-        userDisplayName: this.context.pageContext.user.displayName
+        userDisplayName: this.context.pageContext.user.displayName,
       }
     );
 
     ReactDom.render(element, this.domElement);
   }
 
-  protected onInit(): Promise<void> {
+  protected async onInit(): Promise<void> {
     this._environmentMessage = this._getEnvironmentMessage();
 
-    return super.onInit();
+    await super.onInit();
+    this._spfi = spfi().using(SPFx(this.context));
   }
 
   private _getEnvironmentMessage(): string {
-    if (!!this.context.sdks.microsoftTeams) { // running in Teams
-      return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentTeams : strings.AppTeamsTabEnvironment;
+    if (!!this.context.sdks.microsoftTeams) {
+      // running in Teams
+      return this.context.isServedFromLocalhost
+        ? strings.AppLocalEnvironmentTeams
+        : strings.AppTeamsTabEnvironment;
     }
 
-    return this.context.isServedFromLocalhost ? strings.AppLocalEnvironmentSharePoint : strings.AppSharePointEnvironment;
+    return this.context.isServedFromLocalhost
+      ? strings.AppLocalEnvironmentSharePoint
+      : strings.AppSharePointEnvironment;
   }
 
   protected onDispose(): void {
@@ -54,7 +62,7 @@ export default class ReactZodWebPart extends BaseClientSideWebPart<IReactZodWebP
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -62,20 +70,20 @@ export default class ReactZodWebPart extends BaseClientSideWebPart<IReactZodWebP
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
