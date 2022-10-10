c246d07c6086dc0f86fd9211b91fe96cb22e0db0
AriGunawan <ari.gunawan.3023@gmail.com>
2022-10-11 01:38:03 +1000

    Implement zod

---
+++ samples/react-zod/src/webparts/reactZod/utils/Models.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+const dateFormatter = new Intl.DateTimeFormat([], {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  weekday: "short",
+});
+
+const FormResultSchema = z.object({
+  Id: z.number(),
+  Title: z.string(),
+  Description: z.string(),
+  IsActive: z.boolean(),
+  Qty: z.number().nonnegative(),
+  Rating: z.number().min(0).max(10),
+  Email: z.string().email(),
+  Status: z.enum(["Active", "Done", "Rejected"]),
+  PublishDate: z
+    .string()
+    .transform((value) => (value ? dateFormatter.format(new Date(value)) : "")),
+  Contact: z
+    .object({
+      Title: z.string(),
+    })
+    .transform((value) => value.Title),
+});
+const FormResultsSchema = z.array(FormResultSchema);
+
+type FormResultModel = z.infer<typeof FormResultSchema>;
+type FormResultsModel = z.infer<typeof FormResultsSchema>;
+
+export { FormResultsSchema, FormResultModel, FormResultsModel };
---
+++ samples/react-zod/src/webparts/reactZod/utils/SharePointHelper.ts
@@ -0,0 +1,29 @@
+import "@pnp/sp/webs";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+
+import { SPFI } from "@pnp/sp";
+import { FormResultsModel, FormResultsSchema } from "./Models";
+
+
+export default class SharePointHelper {
+  public static async GetFormResultsData(sp: SPFI): Promise<FormResultsModel> {
+      const items = await sp.web.lists
+        .getByTitle("Form Results")
+        .items
+        .expand('Contact')
+        .select(
+          "Id",
+          "Title",
+          "Description",
+          "Qty",
+          "Rating",
+          "IsActive",
+          "Status",
+          "PublishDate",
+          "Contact/Title",
+          "Email"
+        )();
+      return FormResultsSchema.parse(items);
+  }
+}
