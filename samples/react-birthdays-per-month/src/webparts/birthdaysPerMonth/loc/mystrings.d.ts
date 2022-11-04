7024741f46ed75416da54107d4ded1230a65d632
AriGunawan <ari.gunawan.3023@gmail.com>
2022-11-04 20:28:34 +0700

    Add initial code

---
+++ samples/react-birthdays-per-month/src/webparts/birthdaysPerMonth/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IBirthdaysPerMonthWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'BirthdaysPerMonthWebPartStrings' {
+  const strings: IBirthdaysPerMonthWebPartStrings;
+  export = strings;
+}
