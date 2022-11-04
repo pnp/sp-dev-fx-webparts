c814bbc47c4b69737397e2c74a5c82db91611a88
AriGunawan <ari.gunawan.3023@gmail.com>
2022-11-04 20:28:34 +0700

    Add initial code

---
+++ samples/react-birthdays-per-month/src/webparts/birthdaysPerMonth/components/IBirthdaysPerMonthProps.ts
@@ -0,0 +1,7 @@
+export interface IBirthdaysPerMonthProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
