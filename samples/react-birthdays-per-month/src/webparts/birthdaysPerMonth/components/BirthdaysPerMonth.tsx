79666792a1e9797f05ce4feda01cdee7b7b5204d
AriGunawan <ari.gunawan.3023@gmail.com>
2022-11-05 18:38:28 +0700

    Remove unused code

--- samples/react-birthdays-per-month/src/webparts/birthdaysPerMonth/components/BirthdaysPerMonth.tsx
+++ samples/react-birthdays-per-month/src/webparts/birthdaysPerMonth/components/BirthdaysPerMonth.tsx
@@ -1,43 +1,89 @@
-import * as React from 'react';
-import styles from './BirthdaysPerMonth.module.scss';
-import { IBirthdaysPerMonthProps } from './IBirthdaysPerMonthProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+import styles from "./BirthdaysPerMonth.module.scss";
+import { escape } from "@microsoft/sp-lodash-subset";
 
-export default class BirthdaysPerMonth extends React.Component<IBirthdaysPerMonthProps, {}> {
-  public render(): React.ReactElement<IBirthdaysPerMonthProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
+interface IBirthdaysPerMonthProps {}
 
-    return (
-      <section className={`tw-text-red-700`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
-        </div>
-      </section>
-    );
-  }
-}
+const BirthdaysPerMonth = () => {
+  return (
+    <section className={`tw-text-red-700`}>
+      <div className={styles.welcome}>
+        <img
+          alt=""
+          src={require("../assets/welcome-light.png")}
+          className={styles.welcomeImage}
+        />
+        <h2>Well done, Ari!</h2>
+      </div>
+      <div>
+        <h3>Welcome to SharePoint Framework!</h3>
+        <p>
+          The SharePoint Framework (SPFx) is a extensibility model for Microsoft
+          Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to
+          extend Microsoft 365 with automatic Single Sign On, automatic hosting
+          and industry standard tooling.
+        </p>
+        <h4>Learn more about SPFx development:</h4>
+        <ul className={styles.links}>
+          <li>
+            <a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">
+              SharePoint Framework Overview
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://aka.ms/spfx-yeoman-graph"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Use Microsoft Graph in your solution
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://aka.ms/spfx-yeoman-teams"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Build for Microsoft Teams using SharePoint Framework
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://aka.ms/spfx-yeoman-viva"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Build for Microsoft Viva Connections using SharePoint Framework
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://aka.ms/spfx-yeoman-store"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Publish SharePoint Framework applications to the marketplace
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://aka.ms/spfx-yeoman-api"
+              target="_blank"
+              rel="noreferrer"
+            >
+              SharePoint Framework API reference
+            </a>
+          </li>
+          <li>
+            <a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">
+              Microsoft 365 Developer Community
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export { IBirthdaysPerMonthProps, BirthdaysPerMonth };
--- samples/react-birthdays-per-month/src/webparts/birthdaysPerMonth/components/IBirthdaysPerMonthProps.ts
+++
@@ -1,7 +0,0 @@
-export interface IBirthdaysPerMonthProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
-}
