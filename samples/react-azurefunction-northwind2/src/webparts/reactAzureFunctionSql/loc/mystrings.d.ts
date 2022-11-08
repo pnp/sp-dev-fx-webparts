f2bc5b4dba7d2530aeff7218dbef45765eeacd80
Hugo Bernier <hugoabernier@live.ca>
2022-11-07 22:09:18 -0500

    Added sample from #3031

---
+++ samples/react-azurefunction-northwind2/src/webparts/reactAzureFunctionSql/loc/mystrings.d.ts
@@ -0,0 +1,16 @@
+declare interface IReactAzureFunctionSqlWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AuthLabel: string;
+  FunctionUrl: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ReactAzureFunctionSqlWebPartStrings' {
+  const strings: IReactAzureFunctionSqlWebPartStrings;
+  export = strings;
+}
