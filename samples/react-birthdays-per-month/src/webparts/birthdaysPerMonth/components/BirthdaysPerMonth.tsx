3b6e9a845450131ca5b62aea69c05e947c0edf9a
AriGunawan <ari.gunawan.3023@gmail.com>
2022-12-02 01:59:43 +1000

    Extract functions

--- samples/react-birthdays-per-month/src/webparts/birthdaysPerMonth/components/BirthdaysPerMonth.tsx
+++ samples/react-birthdays-per-month/src/webparts/birthdaysPerMonth/components/BirthdaysPerMonth.tsx
@@ -1,87 +1,17 @@
 import * as React from "react";
-import styles from "./BirthdaysPerMonth.module.scss";
-import { escape } from "@microsoft/sp-lodash-subset";
+import { BirthdaysInMonth } from "../../../models/BirthdaysInMonth";
+import { MonthSection } from "./MonthSection";
 
-interface IBirthdaysPerMonthProps {}
+interface IBirthdaysPerMonthProps {
+  data: Array<BirthdaysInMonth>;
+}
 
-const BirthdaysPerMonth = () => {
+const BirthdaysPerMonth = (props: IBirthdaysPerMonthProps): JSX.Element => {
   return (
-    <section className={`tw-text-red-700`}>
-      <div className={styles.welcome}>
-        <img
-          alt=""
-          src={require("../assets/welcome-light.png")}
-          className={styles.welcomeImage}
-        />
-        <h2>Well done, Ari!</h2>
-      </div>
-      <div>
-        <h3>Welcome to SharePoint Framework!</h3>
-        <p>
-          The SharePoint Framework (SPFx) is a extensibility model for Microsoft
-          Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to
-          extend Microsoft 365 with automatic Single Sign On, automatic hosting
-          and industry standard tooling.
-        </p>
-        <h4>Learn more about SPFx development:</h4>
-        <ul className={styles.links}>
-          <li>
-            <a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">
-              SharePoint Framework Overview
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://aka.ms/spfx-yeoman-graph"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Use Microsoft Graph in your solution
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://aka.ms/spfx-yeoman-teams"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Build for Microsoft Teams using SharePoint Framework
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://aka.ms/spfx-yeoman-viva"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Build for Microsoft Viva Connections using SharePoint Framework
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://aka.ms/spfx-yeoman-store"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Publish SharePoint Framework applications to the marketplace
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://aka.ms/spfx-yeoman-api"
-              target="_blank"
-              rel="noreferrer"
-            >
-              SharePoint Framework API reference
-            </a>
-          </li>
-          <li>
-            <a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">
-              Microsoft 365 Developer Community
-            </a>
-          </li>
-        </ul>
-      </div>
+    <section>
+      {props.data.map((month, index) => (
+        <MonthSection key={month.title} data={month} index={index} />
+      ))}
     </section>
   );
 };
