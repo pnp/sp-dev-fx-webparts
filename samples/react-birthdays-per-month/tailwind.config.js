4f48b292f686b800ff03c4fe2cabb3d8c14c6be2
AriGunawan <ari.gunawan.3023@gmail.com>
2022-12-03 10:50:24 +1000

    Add tailwind base

--- samples/react-birthdays-per-month/tailwind.config.js
+++ samples/react-birthdays-per-month/tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  prefix: 'tw-',
+  prefix: "tw-",
   content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+  corePlugins: {
+    preflight: false,
+  },
+};
