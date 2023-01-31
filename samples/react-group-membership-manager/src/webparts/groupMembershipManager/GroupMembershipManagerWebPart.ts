f3af010335c625db66b1080eb52af32c81ab09e3
Nick Brown <nick@nbdev.co.uk>
2023-01-31 16:07:54 +0000

    Updates FluentUI9 demo and react-group-membership-manager to SPFX 1.16.1 and FluentUI 9.10.1 (#3423)
    Merging conflicts

--- samples/react-group-membership-manager/src/webparts/groupMembershipManager/GroupMembershipManagerWebPart.ts
+++ samples/react-group-membership-manager/src/webparts/groupMembershipManager/GroupMembershipManagerWebPart.ts
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
-import { IPropertyPaneConfiguration } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import { IReadonlyTheme } from '@microsoft/sp-component-base';
 import GroupMembershipManager from './components/GroupMembershipManager';
 import { IGroupMembershipManagerProps } from './components/IGroupMembershipManagerProps';
 import { FluentProvider, FluentProviderProps, teamsDarkTheme, teamsLightTheme, webLightTheme, webDarkTheme, Theme } from '@fluentui/react-components';
+import { createv9Theme } from '../../shims/v9ThemeShim';
 
 export enum AppMode {
-  SharePoint, SharePointLocal, Teams, TeamsLocal
+  SharePoint, Teams, Office, Outlook
 }
 
 export default class GroupMembershipManagerWebPart extends BaseClientSideWebPart<{}> {
@@ -17,9 +17,16 @@ export default class GroupMembershipManagerWebPart extends BaseClientSideWebPart
   private _appMode: AppMode = AppMode.SharePoint;
   private _theme: Theme = webLightTheme;
 
-  protected onInit(): Promise<void> {
-    //on initalizational set the App Mode
-    this._appMode = !!this.context.sdks.microsoftTeams ? (this.context.isServedFromLocalhost ? AppMode.TeamsLocal : AppMode.Teams) : (this.context.isServedFromLocalhost) ? AppMode.SharePointLocal : AppMode.SharePoint;
+  protected async onInit(): Promise<void> {
+    if (!!this.context.sdks.microsoftTeams) {
+      const teamsContext = await this.context.sdks.microsoftTeams.teamsJs.app.getContext();
+      switch (teamsContext.app.host.name.toLowerCase()) {
+        case 'teams': this._appMode = AppMode.Teams; break;
+        case 'office': this._appMode = AppMode.Office; break;
+        case 'outlook': this._appMode = AppMode.Outlook; break;
+        default: throw new Error('Unknown host');
+      }
+    } else this._appMode = AppMode.SharePoint;
     return super.onInit();
   }
 
@@ -32,14 +39,16 @@ export default class GroupMembershipManagerWebPart extends BaseClientSideWebPart
         context: this.context
       }
     );
-
+ 
     //wrap the component with the Fluent UI 9 Provider.
     const fluentElement: React.ReactElement<FluentProviderProps> = React.createElement(
       FluentProvider,
       {
-        theme : this._appMode === AppMode.Teams || this._appMode === AppMode.TeamsLocal ? 
-                  this._isDarkTheme ? teamsDarkTheme : teamsLightTheme : 
-                  this._isDarkTheme ? webDarkTheme : this._theme
+        theme: this._appMode === AppMode.Teams ?
+          this._isDarkTheme ? teamsDarkTheme : teamsLightTheme :
+          this._appMode === AppMode.SharePoint ?
+            this._isDarkTheme ? webDarkTheme : this._theme :
+            this._isDarkTheme ? webDarkTheme : webLightTheme
       },
       element
     );
@@ -50,30 +59,9 @@ export default class GroupMembershipManagerWebPart extends BaseClientSideWebPart
   protected onThemeChanged(currentTheme: IReadonlyTheme | undefined): void {
     if (!currentTheme) return;
     this._isDarkTheme = !!currentTheme.isInverted;
-
     //if the app mode is sharepoint, adjust the fluent ui 9 web light theme to use the sharepoint theme color, teams/dark mode should be fine on default
-    if (this._appMode === AppMode.SharePoint || this._appMode === AppMode.SharePointLocal) {
-      this._theme = {...webLightTheme, 
-        colorBrandBackground: currentTheme.palette.themePrimary,
-        colorBrandBackgroundHover: currentTheme.palette.themeDark,
-        colorBrandBackgroundPressed: currentTheme.palette.themeDarker,
-        colorCompoundBrandForeground1: currentTheme.palette.themePrimary,
-        colorCompoundBrandForeground1Hover: currentTheme.palette.themeDark,
-        colorCompoundBrandForeground1Pressed: currentTheme.palette.themeDarker,
-        colorNeutralForeground2BrandHover: currentTheme.palette.themeSecondary,
-        colorNeutralForeground2BrandPressed: currentTheme.palette.themeDarkAlt,
-        colorNeutralForeground2BrandSelected: currentTheme.palette.themeDarkAlt,
-        colorBrandForeground1: currentTheme.palette.themeSecondary,
-        colorBrandStroke1: currentTheme.palette.themePrimary,
-        colorBrandStroke2: currentTheme.palette.themeSecondary,
-        colorCompoundBrandStroke: currentTheme.palette.themePrimary,
-        colorCompoundBrandStrokeHover: currentTheme.palette.themeSecondary,
-        colorCompoundBrandStrokePressed: currentTheme.palette.themeDarkAlt,
-        colorCompoundBrandBackground: currentTheme.palette.themePrimary,
-        colorCompoundBrandBackgroundHover: currentTheme.palette.themeDark,
-        colorCompoundBrandBackgroundPressed: currentTheme.palette.themeDarker,
-
-      };
+    if (this._appMode === AppMode.SharePoint) {
+      this._theme = createv9Theme(currentTheme, webLightTheme);
     }
   }
 
@@ -83,12 +71,5 @@ export default class GroupMembershipManagerWebPart extends BaseClientSideWebPart
 
   protected get dataVersion(): Version {
     return Version.parse('1.0');
-  }
-
-  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-    return {
-      pages: [
-      ]
-    };
   }
 }
--- samples/react-group-membership-manager/src/webparts/groupMembershipManager/components/DeleteUser.tsx
+++ samples/react-group-membership-manager/src/webparts/groupMembershipManager/components/DeleteUser.tsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 import * as MicrosoftGraph from '@microsoft/microsoft-graph-types';
 import styles from './GroupMembershipManager.module.scss';
 import * as strings from 'GroupMembershipManagerWebPartStrings'
-import { Dialog, DialogTrigger, DialogSurface, DialogTitle, DialogBody, DialogActions, Alert } from "@fluentui/react-components/unstable";
-import { Button, Spinner } from "@fluentui/react-components";
+import { Alert } from "@fluentui/react-components/unstable";
+import { Dialog, DialogTrigger, DialogSurface, DialogTitle, DialogBody, DialogActions, Button, Spinner, DialogContent } from "@fluentui/react-components";
 import { AddUserMode } from './AddUser';
 import { WebPartContext } from '@microsoft/sp-webpart-base';
 import { PersonDeleteRegular } from '@fluentui/react-icons';
 import { MSGraphClientV3 } from '@microsoft/sp-http';
 import { PersonaSize } from 'office-ui-fabric-react/lib/Persona';
 import SPFxPeopleCard from './SPFxPeopleCard';
-import { BatchRequestStep, BatchRequestContent, BatchRequestBody, BatchResponseContent } from '@microsoft/microsoft-graph-client';
+import { BatchRequestStep, BatchRequestContent, BatchRequestBody, BatchResponseContent } from '@microsoft/microsoft-graph-clientv1';
 
 enum rState { Idle, Running, Error, Completed }
 
@@ -41,7 +41,7 @@ export default function RemoveUser({ Group, Users, Mode, context, onCompleted }:
             const userRequestSteps: BatchRequestStep[] = Users.map((v, i) => ({
                 id: i.toString(),
                 request: new Request(`/groups/${Group.id}/${Mode === AddUserMode.Member ? 'members' : 'owners'}/${v.id}/$ref`, {
-                  method: "DELETE"
+                    method: "DELETE"
                 })
             }));
 
@@ -74,33 +74,34 @@ export default function RemoveUser({ Group, Users, Mode, context, onCompleted }:
     }, [running]);
 
 
-    return (
+    return (<>
+        <Button appearance='primary' icon={<PersonDeleteRegular />} disabled={Users.length === 0} onClick={() => setOpen(true)}>{strings.Remove}</Button>
         <Dialog open={open} onOpenChange={(event, data) => setOpen(data.open)}>
-            <DialogTrigger>
-                <Button appearance='primary' icon={<PersonDeleteRegular />} disabled={Users.length === 0}>{strings.Remove}</Button>
-            </DialogTrigger>
             <DialogSurface aria-label="label">
-                <DialogTitle>{Mode === AddUserMode.Owner ? strings.RemoveDialogTitleOwner : strings.RemoveDialogTitle}{Group.displayName}</DialogTitle>
                 <DialogBody>
-                    <div className={styles.stack}>
-                        {running !== rState.Error && _error && <Alert intent="error">{_error.split('||').map((v, i) => (<div key={i}>{v}</div>))}</Alert>}
-                        {running === rState.Error && _error && <Alert intent="error" action={{ children: 'Retry', onClick: () => setRunning(rState.Running) }}>{_error.split('||').map((v, i) => (<div key={i}>{v}</div>))}</Alert>}
-                        {running === rState.Completed && <Alert intent="success">{Mode === AddUserMode.Owner ? strings.Owners : strings.Members} {strings.Removed} {Group.displayName}</Alert>}
-                        {Users.length > 0 && <div className={styles.stackHoz} style={{ flexWrap: 'wrap' }}>
-                            {Users.map(u => <div key={u.id} className={styles.stackHoz} style={{ maxWidth: 200, whiteSpace: 'nowrap' }}>
-                                <SPFxPeopleCard primaryText={u.displayName} serviceScope={context.serviceScope} email={u.userPrincipalName} size={PersonaSize.size24} secondaryText={u.mail} />
-                            </div>)}
-                        </div>}
-                        {running === rState.Running && <Spinner labelPosition='below' label={strings.Removing} />}
-                    </div>
+                    <DialogTitle>{Mode === AddUserMode.Owner ? strings.RemoveDialogTitleOwner : strings.RemoveDialogTitle}{Group.displayName}</DialogTitle>
+
+                    <DialogContent>
+                        <div className={styles.stack}>
+                            {running !== rState.Error && _error && <Alert intent="error">{_error.split('||').map((v, i) => (<div key={i}>{v}</div>))}</Alert>}
+                            {running === rState.Error && _error && <Alert intent="error" action={{ children: 'Retry', onClick: () => setRunning(rState.Running) }}>{_error.split('||').map((v, i) => (<div key={i}>{v}</div>))}</Alert>}
+                            {running === rState.Completed && <Alert intent="success">{Mode === AddUserMode.Owner ? strings.Owners : strings.Members} {strings.Removed} {Group.displayName}</Alert>}
+                            {Users.length > 0 && <div className={styles.stackHoz} style={{ flexWrap: 'wrap' }}>
+                                {Users.map(u => <div key={u.id} className={styles.stackHoz} style={{ maxWidth: 200, whiteSpace: 'nowrap' }}>
+                                    <SPFxPeopleCard primaryText={u.displayName} serviceScope={context.serviceScope} email={u.userPrincipalName} size={PersonaSize.size24} secondaryText={u.mail} />
+                                </div>)}
+                            </div>}
+                            {running === rState.Running && <Spinner labelPosition='below' label={strings.Removing} />}
+                        </div>
+                    </DialogContent>
+                    <DialogActions>
+                        <DialogTrigger>
+                            <Button appearance="secondary" disabled={running === rState.Running}>{strings.Close}</Button>
+                        </DialogTrigger>
+                        <Button appearance="primary" disabled={running === rState.Completed || running === rState.Running} onClick={remove}>{strings.Remove}</Button>
+                    </DialogActions>
                 </DialogBody>
-                <DialogActions>
-                    <DialogTrigger>
-                        <Button appearance="secondary" disabled={running === rState.Running}>{strings.Close}</Button>
-                    </DialogTrigger>
-                    <Button appearance="primary" disabled={running === rState.Completed || running === rState.Running} onClick={remove}>{strings.Remove}</Button>
-                </DialogActions>
             </DialogSurface>
         </Dialog>
-    )
+    </>)
 }
