81ecbc9971fc7e654876db16a76b5a6c8d347a75
João Mendes <joao.j.mendes@outlook.com>
2023-02-01 10:25:50 +0000

    commit change code refratoring remove unused constants

--- samples/react-modern-birthdays/src/constants/constants.ts
+++ samples/react-modern-birthdays/src/constants/constants.ts
@@ -1,30 +1,7 @@
 export const DEFAULT_PAGE_SIZE = 6;
 export const CARD_WIDTH = 256 ;
 export const birthdayListTitle = "Birthdays";
-export const cssLinearGradients =  new Map<string, string> ([
-  ["backGround1","linear-gradient(122.54deg, rgba(40, 176, 206, 0.2) 0%, rgba(192, 88, 255, 0.2) 100%)"  ],
-  ["backGround2","linear-gradient(122.54deg, rgba(145, 61, 255, 0.2) 0%, rgba(255, 234, 122, 0.2) 100%)" ],
-  ["backGround3","linear-gradient(122.54deg, rgba(19, 29, 255, 0.2) 0%, rgba(97, 217, 149, 0.2) 100%)" ],
-  ["backGround4","linear-gradient(122.54deg, rgba(45, 175, 192, 0.2) 39.06%, rgba(112, 255, 0, 0.2) 100%)" ],
-  ["backGround5","linear-gradient(122.54deg, rgba(0, 160, 35, 0.2) 0%, rgba(213, 217, 16, 0.2) 77.6%)" ],
-  ["backGround6","linear-gradient(122.54deg, rgba(243, 187, 46, 0.2) 0%, rgba(200, 0, 0, 0.2) 100%)" ],
-  ["backGround7","linear-gradient(122.54deg, rgba(255, 56, 56, 0.2) 0%, rgba(60, 140, 214, 0.2) 83.33%)" ],
-  ["backGround8","linear-gradient(122.54deg, rgba(255, 162, 22, 0.2) 0%, rgba(204, 0, 255, 0.2) 100%)" ],
-]);
 
-export const teamsBackgroundImage = new Map<string, string> ([
-  ["backGround1","https://praise.myanalytics.cdn.office.net/2022.12.28.1/assets/themes/CornflowerGradient.png" ],
-  ["backGround2","https://praise.myanalytics.cdn.office.net/2022.12.28.1/assets/themes/FloralGradient.png" ],
-  ["backGround3","https://praise.myanalytics.cdn.office.net/2022.12.28.1/assets/themes/BabyBlueGradient.png"  ],
-  ["backGround4","https://praise.myanalytics.cdn.office.net/2022.12.28.1/assets/themes/EmeraldGradient.png" ],
-  ["backGround5","https://praise.myanalytics.cdn.office.net/2022.12.28.1/assets/themes/LimeGradient.png" ],
-  ["backGround6","https://praise.myanalytics.cdn.office.net/2022.12.28.1/assets/themes/TuscanyGradient.png" ],
-  ["backGround7","https://praise.myanalytics.cdn.office.net/2022.12.28.1/assets/themes/PeachGradient.png" ],
-  ["backGround8","https://praise.myanalytics.cdn.office.net/2022.12.28.1/assets/themes/FlamingoGradient.png" ],
-
-]);
-
-export const teamsCongratulationsImage =  "https://praise.myanalytics.cdn.office.net/2022.12.28.1/assets/emojis/PartyPopper_Icon.png"
 
 
 
