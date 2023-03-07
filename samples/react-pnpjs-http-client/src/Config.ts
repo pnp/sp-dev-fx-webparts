070bdfdf4d200e3fa78c91d56a597e88e5cf8461
Divya Akula <divyaa@sp.dtisbank.is>
2023-03-07 22:14:56 +0530

    added pnpjsv3 sample that invoke a github api with fetch of SPHttpclient

---
+++ samples/react-pnpjs-http-client/src/Config.ts
@@ -0,0 +1,63 @@
+import { WebPartContext } from "@microsoft/sp-webpart-base";
+
+// import pnp and pnp logging system
+import {  SPFI } from "@pnp/sp";
+import { LogLevel, PnPLogging } from "@pnp/logging";
+import "@pnp/sp/webs";
+import "@pnp/sp/lists";
+import "@pnp/sp/items";
+import "@pnp/sp/batching";
+import { DefaultHeaders, GraphFI,  } from "@pnp/graph";
+import { SPHttpClient } from '@microsoft/sp-http';
+import { AdalClient } from "@pnp/adaljsclient";
+import "@microsoft/sp-http";
+import { spfi, SPFx as spSPFx } from "@pnp/sp";
+import { graphfi, SPFx as graphSPFx} from "@pnp/graph";
+// eslint-disable-next-line no-var
+var _sp: SPFI = null;
+var _graph:GraphFI=null;
+var _SpHttpClient:SPHttpClient = null;
+var _adalClient: any = null;
+
+export const getSP = (context?: WebPartContext): SPFI => {
+  if (context != null) { // eslint-disable-line eqeqeq
+    //You must add the @pnp/logging package to include the PnPLogging behavior it is no longer a peer dependency
+    // The LogLevel set's at what level a message will be written to the console
+    _sp = spfi(
+      
+    ).using(spSPFx(context)).using().using(PnPLogging(LogLevel.Warning));
+    // );
+    // context.aadHttpClientFactory= new AdalClient("c87e47c6-5554-41a0-86b6-d92a11dfc8b9","fb6f17d5-3625-4013-8cd0-13d6ed4ba991","https://isbsharepointtest.sharepoint.com/sites/Test-Syntex-Document-Processor");
+    // _SpHttpClient =context.spHttpClient;
+    
+  }
+  return _sp;
+};
+
+export const getGraph = (context?: WebPartContext): GraphFI => {
+  
+  // _sp = spfi().using(SPFx(context)).using(PnPLogging(LogLevel.Warning));
+  
+  // _sp = spfi().using(SPFx(context)).using(PnPLogging(LogLevel.Warning));
+  // _sp = spfi().using(SPFx(context)).using(PnPLogging(LogLevel.Warning));
+  // _sp = spfi().using(SPFx(context)).using(PnPLogging(LogLevel.Warning));
+  if (context != null) { 
+  _graph = graphfi().using(graphSPFx(context)).using(DefaultHeaders());
+  }
+
+return _graph;
+};
+export const getSPHttpClient =(context?: WebPartContext):SPHttpClient=>{
+  if (context != null) {
+    _SpHttpClient = context.spHttpClient;
+
+  }
+  return _SpHttpClient;
+};
+export const getAdalClient =(context?: WebPartContext):any=>{
+  if (context != null) {
+    _adalClient = AdalClient.fromSPFxContext(context);
+
+  }
+  return _adalClient;
+};
