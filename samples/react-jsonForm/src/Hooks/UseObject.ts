4ddaad7896b15fcdc99d0c61e38fa182324e7576
Dan Toft <tanddant@gmail.com>
2023-05-16 20:25:29 +0200

    🎉 - [react-json-form]: Load submitted form | rename folder | work on readme

--- samples/react-jsonForm/src/Hooks/UseObject.ts
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-export default function useObject<T>(InitialValue?: T) {
-    const [value, setValue] = useState<T>(InitialValue ?? {} as T);
-    const updateValue: (Updates: Partial<T>) => void = (Updates: Partial<T>) => setValue((prev) => ({ ...prev, ...Updates }))
-    return {
-        value, updateValue
-    };
-}
