7721546ff9dea86290f4c0aab8a860bc86a24783
Dan Toft <tanddant@gmail.com>
2023-05-16 21:04:45 +0200

    🤦‍♂️ - [React-json-form]: Git is hard, think I made it back

--- samples/react-json-form/src/Hooks/UseObject.ts
+++ samples/react-json-form/src/Hooks/UseObject.ts
@@ -4,6 +4,6 @@ export default function useObject<T>(InitialValue?: T) {
     const [value, setValue] = useState<T>(InitialValue ?? {} as T);
     const updateValue: (Updates: Partial<T>) => void = (Updates: Partial<T>) => setValue((prev) => ({ ...prev, ...Updates }))
     return {
-        value, updateValue
+        value, updateValue, overwriteData: setValue
     };
 }
