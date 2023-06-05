4fded36d79c3fb8e25ce7a513f50d05d2819f49b
Hugo Bernier <hugoabernier@live.ca>
2023-06-05 09:55:24 -0400

    Revert "Samples/react dashboards"

--- samples/react-dashboards/src/components/DatePickerMenu/IDatePickerMenuProps.ts
+++
@@ -1,8 +0,0 @@
-﻿import { IDropdownOption } from "@fluentui/react";
-
-export interface IDatePickerMenuProps {
-    onDateSelected: (dateSelection: string) => void;
-    timeSpanMenus: IDropdownOption[];
-    initialValue: string;
-    cultureName: string;
-}
--- samples/react-dashboards/src/components/SwatchColorPickerThemed/ISwatchColorPickerThemedProps.ts
+++
@@ -1,8 +0,0 @@
-﻿import { IChoiceGroupOption } from "@fluentui/react";
-import { ThemedPalette } from "../../common/ColorsHelper";
-
-export interface ISwatchColorPickerThemedProps {
-    configThemePalette: ThemedPalette[];
-    themePalette: string
-    onChangeColorPalette: (ev?: React.FormEvent<HTMLElement | HTMLInputElement>, option?: IChoiceGroupOption) => void;
-}
