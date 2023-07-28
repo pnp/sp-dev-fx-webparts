df121350500884f1aeab45036186a4da9c913174
AriGunawan <ari.gunawan.3023@gmail.com>
2023-07-28 22:30:51 +0700

    Initial generated code

---
+++ samples/react-enhanced-page-properties/src/webparts/enhancedPageProperties/components/IEnhancedPagePropertiesProps.ts
@@ -0,0 +1,7 @@
+export interface IEnhancedPagePropertiesProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
