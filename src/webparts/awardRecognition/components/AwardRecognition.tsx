cd9f4c3450b75d09d295fa234d1de90e68804eed
Sandeep P S <sandeep.ps@quadrasystems.net>
2023-11-18 16:10:43 +0530

    feat: create context for passing webpartContext across all components

--- src/webparts/awardRecognition/components/AwardRecognition.tsx
+++ src/webparts/awardRecognition/components/AwardRecognition.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
-// import styles from "./AwardRecognition.module.scss";
 import { IAwardRecognitionProps } from "./IAwardRecognitionProps";
 import { FluentProvider, webLightTheme } from "@fluentui/react-components";
 import { Carousel } from "../../../components/Carousel";
+import { createContext } from "react";
+
+export const WebpartContext = createContext<IAwardRecognitionProps>(null);
 
 export const AwardRecognition = (props: IAwardRecognitionProps) => {
   return (
-    <FluentProvider theme={webLightTheme}>
-      <Carousel />
-    </FluentProvider>
+    <WebpartContext.Provider value={props}>
+      <FluentProvider
+        theme={webLightTheme}
+        style={{ width: "100%", background: "none" }}
+      >
+        <Carousel />
+      </FluentProvider>
+    </WebpartContext.Provider>
   );
 };
