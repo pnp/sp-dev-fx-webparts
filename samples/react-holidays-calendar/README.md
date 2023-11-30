8a4bb20ad46c61a469205342e4ae13ce4448376b
Harminder Singh <harminder.singh01@nagarro.com>
2023-11-30 23:18:19 +0530

    Upgraded react-holiday-calender to SPFx 1.18.2

--- samples/react-holidays-calendar/README.md
+++ samples/react-holidays-calendar/README.md
@@ -26,8 +26,8 @@ ACE card extension provides below functionalities
 
 This sample is optimally compatible with the following environment configuration:
 
-![SPFx 1.16.1](https://img.shields.io/badge/SPFx-1.16.1-green.svg)
-![Node.js v16 | v14 | v12](https://img.shields.io/badge/Node.js-v16%20%7C%20v14%20%7C%20v12-green.svg)
+![SPFx 1.18.2](https://img.shields.io/badge/SPFx-1.18.2-green.svg)
+![Node.js v18 | v18](https://img.shields.io/badge/Node.js-v18%20%7C%20v16-green.svg)
 ![Compatible with SharePoint Online](https://img.shields.io/badge/SharePoint%20Online-Compatible-green.svg)
 ![Does not work with SharePoint 2019](https://img.shields.io/badge/SharePoint%20Server%202019-Incompatible-red.svg "SharePoint Server 2019 requires SPFx 1.4.1 or lower")
 ![Does not work with SharePoint 2016 (Feature Pack 2)](https://img.shields.io/badge/SharePoint%20Server%202016%20(Feature%20Pack%202)-Incompatible-red.svg "SharePoint Server 2016 Feature Pack 2 requires SPFx 1.1")
@@ -58,6 +58,7 @@ For more information about SPFx compatibility, please refer to <https://aka.ms/s
 | Version | Date             | Comments        |
 | ------- | ---------------- | --------------- |
 | 1.0     | January 29, 2021 | Initial release |
+| 1.1     | November 30, 2023 | Upgraded to SPFx 1.18.2 |
 
 ## Minimal Path to Awesome
 
--- samples/react-holidays-calendar/src/common/services/GraphService.ts
+++ samples/react-holidays-calendar/src/common/services/GraphService.ts
@@ -1,38 +1,38 @@
+import { MSGraphClientV3 } from "@microsoft/sp-http-msgraph";
+
 /* eslint-disable */
 
 import { Event as IEventType, User } from "@microsoft/microsoft-graph-types";
 
-import { MSGraphClientV3, GraphRequest } from "@microsoft/sp-http-msgraph";
-
 export class GraphService {
-	private context: any;
+  private context: any;
 
-	constructor(context: any) {
-		this.context = context;
-	}
+  constructor(context: any) {
+    this.context = context;
+  }
 
-	private getClient = async (): Promise<MSGraphClientV3> => {
-		return await this.context.msGraphClientFactory.getClient("3");
-	};
-	public getMyInformation = async () => {
-		const client = await this.getClient();
-		const request: GraphRequest = client.api("/me");
-		const employeeInfo: User = await request.get();
-		return Promise.resolve(employeeInfo);
-	};
-	public getMyTimeZone = async (): Promise<string> => {
-		const client = await this.getClient();
-		const request: GraphRequest = client.api("/me/mailboxSettings");
-		const response = await request.get();
-		return Promise.resolve(response.timeZone);
-	};
+  private getClient = async (): Promise<MSGraphClientV3> => {
+    return await this.context.msGraphClientFactory.getClient("3");
+  };
+  public getMyInformation = async () => {
+    const client = await this.getClient();
+    const request = client.api("/me");
+    const employeeInfo: User = await request.get();
+    return Promise.resolve(employeeInfo);
+  };
+  public getMyTimeZone = async (): Promise<string> => {
+    const client = await this.getClient();
+    const request = client.api("/me/mailboxSettings");
+    const response = await request.get();
+    return Promise.resolve(response.timeZone);
+  };
 
-	public createEvent = async (detail: IEventType): Promise<any> => {
-		const client = await this.getClient();
-		const request: GraphRequest = client.api("/me/events");
+  public createEvent = async (detail: IEventType): Promise<any> => {
+    const client = await this.getClient();
+    const request = client.api("/me/events");
 
-		const response = await request.post(detail);
+    const response = await request.post(detail);
 
-		return Promise.resolve(response);
-	};
+    return Promise.resolve(response);
+  };
 }
