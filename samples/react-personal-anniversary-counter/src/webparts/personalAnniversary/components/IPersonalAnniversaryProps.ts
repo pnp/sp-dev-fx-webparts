52b55da4dd5e0c917950e3ed4939961e61d0d592
Dan Toft <tanddant@gmail.com>
2023-12-09 16:10:38 +0100

    🎉 - [react-personal-anniversary-counter] Init new sample

---
+++ samples/react-personal-anniversary-counter/src/webparts/personalAnniversary/components/IPersonalAnniversaryProps.ts
@@ -0,0 +1,7 @@
+export interface IPersonalAnniversaryProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
