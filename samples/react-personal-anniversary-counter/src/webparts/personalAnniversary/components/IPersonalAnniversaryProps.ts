dc6517574171c6e6663123f9673ec9167cf7fdbc
Dan Toft <tanddant@gmail.com>
2023-12-09 16:14:20 +0100

    🧹 - [react-personal-anniversary-counter] Remove code that won't be used

--- samples/react-personal-anniversary-counter/src/webparts/personalAnniversary/components/IPersonalAnniversaryProps.ts
+++
@@ -1,7 +0,0 @@
-export interface IPersonalAnniversaryProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
-}
