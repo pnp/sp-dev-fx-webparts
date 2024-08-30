bc3d9f7b38e9a4f6294c0e04a84635627b947e02
AriGunawan <ari.gunawan.3023@gmail.com>
2024-08-30 23:04:56 +0700

    Initial generated code

---
+++ samples/react-enhanced-button/src/webparts/enhancedButton/components/IEnhancedButtonProps.ts
@@ -0,0 +1,7 @@
+export interface IEnhancedButtonProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ samples/react-enhanced-button/src/webparts/enhancedButton/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IEnhancedButtonWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'EnhancedButtonWebPartStrings' {
+  const strings: IEnhancedButtonWebPartStrings;
+  export = strings;
+}
