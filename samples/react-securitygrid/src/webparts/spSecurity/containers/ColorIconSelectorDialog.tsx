693640944f5596bbf274c7e3f25ae877b83f7dbb
D'Andrea Nello <nello.dandrea@mobi.ch>
2024-10-20 18:52:55 +0200

    Upgrade to SPFx 1.20

--- samples/react-securitygrid/src/webparts/spSecurity/containers/ColorIconSelectorDialog.tsx
+++ samples/react-securitygrid/src/webparts/spSecurity/containers/ColorIconSelectorDialog.tsx
@@ -1,92 +1,47 @@
 import * as React from 'react';
-import { Button, PrimaryButton } from 'office-ui-fabric-react/lib/Button';
-import { Dropdown, IDropdownOption } from "office-ui-fabric-react/lib/Dropdown";
-import { ColorPicker } from "office-ui-fabric-react/lib/ColorPicker";
-import { IColor } from "office-ui-fabric-react/lib/Color";
-import { Dialog } from "office-ui-fabric-react/lib/Dialog";
-import { TextField } from "office-ui-fabric-react/lib/TextField";
-import { Icon } from "office-ui-fabric-react/lib/Icon";
-import { ComboBox, IComboBox, IComboBoxOption, } from "office-ui-fabric-react/lib/ComboBox";
-import { Label } from 'office-ui-fabric-react/lib/Label';
+import { Button, PrimaryButton } from '@fluentui/react/lib/Button';
+import { Dropdown, IDropdownOption } from "@fluentui/react/lib/Dropdown";
+import { ColorPicker } from "@fluentui/react/lib/ColorPicker";
+import { IColor } from "@fluentui/react/lib/Color";
+import { Dialog } from "@fluentui/react/lib/Dialog";
+import { TextField } from "@fluentui/react/lib/TextField";
+import { Icon } from "@fluentui/react/lib/Icon";
+import { ComboBox, IComboBoxOption } from "@fluentui/react/lib/ComboBox";
+import { Label } from '@fluentui/react/lib/Label';
 import { ISelectedPermission } from "../ISpSecurityWebPartProps";
 import { SPPermission } from "@microsoft/sp-page-context";
 import { findIndex } from "lodash";
+
 export interface IColorIconSelectorPanelProps {
   isOpen: boolean;
-  onPermissionChange(color: ISelectedPermission): void;
-  closePanel(): void;
+  onPermissionChange: (perm: ISelectedPermission) => void;
+  closePanel: () => void;
   title: string;
   subText: string;
   currentPerm: ISelectedPermission;
   SelectedPermissions: ISelectedPermission[];
 }
+
 export interface IColorIconSelectionPanelState {
   currentPerm: ISelectedPermission;
 }
+
 export default class ColorIconSelectionPanel extends React.Component<IColorIconSelectorPanelProps, IColorIconSelectionPanelState> {
 
-  private iconNames = [
-    "Add",
-    "Back",
-    "BlockContact",
-    "Cancel",
-    "Cat",
-    "CheckBox",
-    "CheckBoxComposite",
-    "CheckMark",
-    "ChevronDown",
-    "ChevronUp",
-    "CircleFill",
-    "CircleRing",
-    "Clear",
-    "Contact",
-    "Contrast",
-    "Delete",
-    "Design",
-    "eDiscovery",
-    "Edit",
-    "EditSolid12",
-    "Emoji2",
-    "Error",
-    "FavoriteStar",
-    "FavoriteStarFill",
-    "Glasses",
-    "HomeSolid",
-    "IncidentTriangle",
-    "LaptopSecure",
-    "LifeSaver",
-    "LifeSaverLock",
-    "Lock",
-    "LockSolid",
-    "Mail",
-    "More",
-    "PageLink",
-    "People",
-    "PeopleAdd",
-    "Permissions",
-    "PinSolid12",
-    "RedEye",
-    "Sad",
-    "SecurityGroup",
-    "Settings",
-    "Share",
-    "Sync",
-    "TriangleSolid",
-    "UnEditable",
-    "UnEditable2",
-    "Unlock",
-    "UserWarning",
-    "View",
-
-
-
+  private iconNames: string[] = [
+    "Add", "Back", "BlockContact", "Cancel", "Cat", "CheckBox", "CheckBoxComposite", "CheckMark",
+    "ChevronDown", "ChevronUp", "CircleFill", "CircleRing", "Clear", "Contact", "Contrast", "Delete",
+    "Design", "eDiscovery", "Edit", "EditSolid12", "Emoji2", "Error", "FavoriteStar", "FavoriteStarFill",
+    "Glasses", "HomeSolid", "IncidentTriangle", "LaptopSecure", "LifeSaver", "LifeSaverLock", "Lock",
+    "LockSolid", "Mail", "More", "PageLink", "People", "PeopleAdd", "Permissions", "PinSolid12", "RedEye",
+    "Sad", "SecurityGroup", "Settings", "Share", "Sync", "TriangleSolid", "UnEditable", "UnEditable2",
+    "Unlock", "UserWarning", "View"
   ];
+
   constructor(props: IColorIconSelectorPanelProps) {
     super(props);
-    //debugger;
-
     this.state = {
-      currentPerm: this.props.currentPerm
+      currentPerm: { ...this.props.currentPerm } // Ensure immutability of props
     };
   }
 
@@ -95,114 +50,97 @@ export default class ColorIconSelectionPanel extends React.Component<IColorIconS
     this.onClosePanel();
   }
 
-  private onClosePanel(element?: any): void {
+  private onClosePanel(): void {
     this.props.closePanel();
   }
-  private renderIcon(props?, defaultRender?: (props?) => JSX.Element | null): JSX.Element {
 
-    return (<div>
-      <Icon iconName={props.text} /> {props.text}
-    </div>);
+  private renderIcon(option?: IComboBoxOption): JSX.Element {
+    return (
+      <div>
+        <Icon iconName={option?.text} /> {option?.text}
+      </div>
+    );
   }
-
-
-
 
   private getIconOptions(): IComboBoxOption[] {
-    let options: IComboBoxOption[] = [];
-    for (var iconName of this.iconNames) {
-      var option: IComboBoxOption = {
-        key: iconName, text: iconName,
-      };
-      options.push(option);
-    }
-    return options;
+    return this.iconNames.map(iconName => ({
+      key: iconName,
+      text: iconName
+    }));
   }
+
   public getPermissionTypes(): IDropdownOption[] {
-    let perms = new Array<IDropdownOption>();
-    for (const perm in SPPermission) {
-      if (typeof (SPPermission[perm]) === "object") {
-        perms.push({
-          text: perm,
-          key: perm,
-          disabled: findIndex(this.props.SelectedPermissions, (sp: ISelectedPermission) => { return sp.permission == perm; }) !== -1
-        });
+    return Object.keys(SPPermission).map((perm) => {
+      const isDisabled = findIndex(this.props.SelectedPermissions, sp => sp.permission === perm) !== -1;
+      if (typeof SPPermission[perm as keyof typeof SPPermission] === "object") {
+        return { text: perm, key: perm, disabled: isDisabled };
       }
-    }
-    return perms;
+      return null;
+    }).filter(Boolean) as IDropdownOption[]; // Remove null values
   }
+
   public render(): JSX.Element {
+    const { currentPerm } = this.state;
 
-    //Renders content
     return (
-
       <Dialog
         isOpen={this.props.isOpen}
-        onDismiss={() => this.onClosePanel()}
+        onDismiss={this.onClosePanel.bind(this)}
         title={this.props.title}
         subText={this.props.subText}
       >
-        <Dropdown label="Permission"
+        <Dropdown
+          label="Permission"
           options={this.getPermissionTypes()}
-          defaultSelectedKey={this.state.currentPerm.permission}
-          onChange={(event: React.FormEvent<HTMLDivElement>, option?: IDropdownOption, index?: number) => {
-            this.state.currentPerm.permission = option.text;
-            this.setState((current) => ({ ...current, currentPerm: this.state.currentPerm }));
-          }}>
-        </Dropdown>
-        <TextField label="Friendly Name"
-          defaultValue={this.state.currentPerm.freindlyName}
-          onChange={(event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
-            this.state.currentPerm.freindlyName = newValue;
-            this.setState((current) => ({ ...current, currentPerm: this.state.currentPerm }));
-          }}>
-        </TextField>
-        <Label>Color:</Label>
-        <ColorPicker
-          color={this.state.currentPerm.color}
-          onChange={(event: React.FormEvent<HTMLDivElement>, color: IColor) => {
-            this.state.currentPerm.color = color.str;
-            this.setState((current) => ({ ...current, currentPerm: this.state.currentPerm }));
-          }}>
-        </ColorPicker>
-        <ComboBox allowFreeform={true}
-
-          label="Select Icon(or enter name of a Fabric Icon):"
-          options={this.getIconOptions()}
-          onRenderOption={this.renderIcon}
-          text={this.state.currentPerm.iconName}
-          onChange={(event: React.FormEvent<IComboBox>, option?: IComboBoxOption, index?: number, value?: string) => {
-            //debugger;
-            this.state.currentPerm.iconName = value ? value : option.text;
-            this.setState((current) => ({ ...current, currentPerm: this.state.currentPerm }));
+          defaultSelectedKey={currentPerm.permission}
+          onChange={(event, option) => {
+            const updatedPerm = { ...currentPerm, permission: option?.text || '' };
+            this.setState({ currentPerm: updatedPerm });
           }}
         />
-
+        <TextField
+          label="Freindly Name"
+          defaultValue={currentPerm.freindlyName}
+          onChange={(event, newValue) => {
+            const updatedPerm = { ...currentPerm, freindlyName: newValue || '' };
+            this.setState({ currentPerm: updatedPerm });
+          }}
+        />
+        <Label>Color:</Label>
+        <ColorPicker
+          color={currentPerm.color}
+          onChange={(event, color: IColor) => {
+            const updatedPerm = { ...currentPerm, color: color.str };
+            this.setState({ currentPerm: updatedPerm });
+          }}
+        />
+        <ComboBox
+          allowFreeform
+          label="Select Icon (or enter name of a Fabric Icon):"
+          options={this.getIconOptions()}
+          onRenderOption={this.renderIcon.bind(this)}
+          text={currentPerm.iconName}
+          onChange={(event, option, index, value) => {
+            const updatedPerm = { ...currentPerm, iconName: value || option?.text || '' };
+            this.setState({ currentPerm: updatedPerm });
+          }}
+        />
         <Label>Display:</Label>
-        <Icon iconName={this.state.currentPerm.iconName} style={{ color: this.state.currentPerm.color }} />
-        <br></br>
-        <br></br>
-        <Button onClick={() => {
-          this.onClosePanel();
-        }}>Cancel</Button>
+        <Icon iconName={currentPerm.iconName} style={{ color: currentPerm.color }} />
+        <br />
+        <Button onClick={this.onClosePanel.bind(this)}>Cancel</Button>
         <PrimaryButton
           disabled={
-            this.state.currentPerm.color == null ||
-            this.state.currentPerm.freindlyName == null ||
-            this.state.currentPerm.iconName == null ||
-            this.state.currentPerm.permission == null
+            !currentPerm.color ||
+            !currentPerm.freindlyName ||
+            !currentPerm.iconName ||
+            !currentPerm.permission
           }
-          onClick={() => {
-            //debugger;
-            this.saveChanges();
-          }}
-        >Save</PrimaryButton>
-
+          onClick={this.saveChanges.bind(this)}
+        >
+          Save
+        </PrimaryButton>
       </Dialog>
-
-
     );
   }
 }
-
-
