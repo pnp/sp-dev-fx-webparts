19b4bfea50070f11d510043712100339f5d7ebb5
mohammadamer <m.amer@live.com>
2024-10-25 02:33:52 +0300

    Upgraded react-datatable SPFx to version 1.20.0 (#5312)
    * Upgraded to spfx 1.20.0
    
    * Updated node version to node 18
    
    ---------
    
    Co-authored-by: Mohammad Amer <mohammad.amer@atea.com>

--- samples/react-datatable/src/shared/common/ExportListItemsToPDF/ExportListItemsToPDF.tsx
+++ samples/react-datatable/src/shared/common/ExportListItemsToPDF/ExportListItemsToPDF.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
 import * as strings from 'ReactDatatableWebPartStrings';
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
-pdfMake.vfs = pdfFonts.pdfMake.vfs;
 import { IIconProps, PrimaryButton } from 'office-ui-fabric-react';
 import styles from './ExportListItemsToPDF.module.scss';
 import { isNullOrUndefined } from '../../utilities/utilities';
-import { IPropertyPaneDropdownOption } from '@microsoft/sp-property-pane';
+import * as pdfMake from "pdfmake/build/pdfmake";
+import * as pdfFonts from "pdfmake/build/vfs_fonts";
+import { Content } from "pdfmake/interfaces";
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
 interface IExportToPDF {
     listName: string;
@@ -14,7 +14,7 @@ interface IExportToPDF {
     columns: any[];
     oddRowColor?: string;
     evenRowColor?: string;
-    dataSource: ()=> any[];
+    dataSource: () => any[];
 }
 
 export function ExportListItemsToPDF(props: IExportToPDF) {
@@ -24,9 +24,26 @@ export function ExportListItemsToPDF(props: IExportToPDF) {
     let { title, listName, columns, oddRowColor, evenRowColor, dataSource } = props;
 
     function genearatePDF() {
-
-        let dataTableRows = dataSource().map(lItem => columns.reduce((arr, c) => [...arr, isNullOrUndefined(lItem[c]) ? '' : lItem[c]], []));
-
+        let dataTableRows = dataSource().map(lItem => 
+            columns.reduce((arr, c) => {
+                const value = lItem[c];
+                // Check for null or undefined values
+                if (isNullOrUndefined(value)) {
+                    arr.push('');
+                } 
+                // Check if the value is a React element (such as a div with dangerouslySetInnerHTML)
+                else if (typeof value === 'object' && value.$$typeof === Symbol.for('react.element')) {
+                    //arr.push(value);  // If it's a React element, add it directly to the array
+                    arr.push(value.props.dangerouslySetInnerHTML.__html);
+                } 
+                // Handle other cases (plain strings, numbers, etc.)
+                else {
+                    arr.push(value);
+                }
+        
+                return arr;
+            }, [])
+        );
         let data = {
             content: [
                 {
@@ -34,26 +51,26 @@ export function ExportListItemsToPDF(props: IExportToPDF) {
                     fontSize: 16,
                     alignment: 'center',
                     margin: [0, 0, 0, 15]
-                },
+                } as Content,
                 {
                     style: 'tableExample',
                     table: {
                         widths: new Array(columns.length).fill("auto"),
                         headerRows: 1,
                         body: [
-                            columns.map(c=> ({text: c, bold: true})),
+                            columns.map(c => ({ text: c, bold: true })),
                             ...dataTableRows
                         ]
                     },
                     layout: {
-                        fillColor: (rowIndex: number)=> {
+                        fillColor: (rowIndex: number) => {
                             if (oddRowColor && evenRowColor)
                                 return (rowIndex % 2 === 0) ? evenRowColor : oddRowColor;
                             else
                                 return (rowIndex % 2 === 0) ? '#CCCCCC' : null;
                         }
                     }
-                }
+                } as Content
             ]
         };
         pdfMake.createPdf(data).download(`${listName}.pdf`);
