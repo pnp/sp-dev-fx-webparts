ffd0d1e65a90bcc5ff6fef2f8da4bef338e400b4
Luise Freese <luise@raeuberleiterin.de>
2024-11-12 12:15:00 +0100

    :tada: first draft of bluesky web part

---
+++ samples/react-bluesky/src/webparts/blueSky/loc/mystrings.d.ts
@@ -0,0 +1,21 @@
+declare interface IBlueSkyWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'BlueSkyWebPartStrings' {
+  const strings: IBlueSkyWebPartStrings;
+  export = strings;
+}
+
+declare module '@atproto/api';
