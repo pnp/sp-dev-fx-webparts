eaf3b3a3b32259047c3395ad435e255f6b968127
Sandeep P S <sandymankara11@gmail.com>
2024-12-10 13:18:34 +0530

    feat: use custom fonts

--- samples/react-modern-carousel/src/components/Content.tsx
+++ samples/react-modern-carousel/src/components/Content.tsx
@@ -1,31 +1,35 @@
-import * as React from "react";
-import { Text } from "@fluentui/react-components";
-import styles from "../webparts/awardRecognition/components/AwardRecognition.module.scss";
+import * as React from "react"
+import { Text } from "@fluentui/react-components"
+import styles from "../webparts/awardRecognition/components/AwardRecognition.module.scss"
 import { ContentProps } from "../types/ContentProps"
-import { useContext } from "react";
-import { WebpartContext } from "../webparts/awardRecognition/components/AwardRecognition";
+import { useContext } from "react"
+import { WebpartContext } from "../webparts/awardRecognition/components/AwardRecognition"
 
 export const Content = ({ user }: ContentProps): JSX.Element => {
-  const contextInfo = useContext(WebpartContext);
+  const contextInfo = useContext(WebpartContext)
 
   return (
     <section className={styles.contentWrapper}>
-      <Text size={600} weight="semibold">
+      <Text size={600} weight='semibold' className={styles.webpartTitle}>
         {contextInfo.webpartTitle}
       </Text>
       <div className={styles.contentDescriptionWrapper}>
-        <Text size={400} weight="semibold" className={styles.contentTitle}>
+        <Text size={400} weight='semibold' className={styles.contentTitle}>
           {contextInfo.contentTitle}
         </Text>
-        <Text size={300}>{contextInfo.contentDescription}</Text>
+        <Text size={300} className={styles.contentDescription}>
+          {contextInfo.contentDescription}
+        </Text>
       </div>
 
       <div className={styles.userInfo}>
-        <Text size={800} weight="semibold" className={styles.userName}>
+        <Text size={800} weight='semibold' className={styles.userName}>
           {user?.Title}
         </Text>
-        <Text size={400}>{user?.Designation}</Text>
+        <Text size={400} className={styles.designation}>
+          {user?.Designation}
+        </Text>
       </div>
     </section>
-  );
-};
+  )
+}
