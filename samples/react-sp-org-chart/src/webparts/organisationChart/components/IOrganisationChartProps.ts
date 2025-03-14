430eaa7564edab37b65633b68c9396c35c0c0fdf
Sudeep Ghatak <sudeep.ghatak@theta.co.nz>
2025-03-14 17:47:13 +1300

    Org Chart Base code

---
+++ samples/react-sp-org-chart/src/webparts/organisationChart/components/IOrganisationChartProps.ts
@@ -0,0 +1,7 @@
+export interface IOrganisationChartProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
