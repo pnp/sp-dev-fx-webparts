ba1508e8462680144f7f38046297b86bb08ee237
Sudeep Ghatak <sudeep.ghatak@gmail.com>
2025-09-02 18:24:18 +1200

    Base code

---
+++ samples/react-flag-template/src/webparts/reactFlagTemplate/components/IReactFlagTemplateProps.ts
@@ -0,0 +1,7 @@
+export interface IReactFlagTemplateProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
