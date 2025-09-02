17bebedb55873a22a94b6f175c8f3cfc360ae7b7
Sudeep Ghatak <sudeep.ghatak@gmail.com>
2025-09-02 20:53:32 +1200

    Flag banner

--- samples/react-flag-template/src/webparts/reactFlagTemplate/components/ReactFlagTemplate.tsx
+++ samples/react-flag-template/src/webparts/reactFlagTemplate/components/ReactFlagTemplate.tsx
@@ -1,43 +1,90 @@
+
+
 import * as React from 'react';
+import { IReactFlagTemplateProps } from './IReactFlagTemplateProps';
 import styles from './ReactFlagTemplate.module.scss';
-import type { IReactFlagTemplateProps } from './IReactFlagTemplateProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+const sx = styles as unknown as Record<string, string>;
+
+
+const flags = [
+  {
+    colorClass: sx.flagColor1,
+    icon: (
+      <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="12" y="12" width="40" height="40" rx="8" stroke="white" strokeWidth="4" fill="none" />
+        <path d="M32 20L44 28V44H20V28L32 20Z" stroke="white" strokeWidth="3" fill="none" />
+      </svg>
+    ),
+    title: 'Codepen',
+    descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta, impedit?'
+  },
+  {
+    colorClass: sx.flagColor2,
+    icon: (
+      <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M16 16H48L44 48L32 52L20 48L16 16Z" fill="white" fillOpacity="0.1" />
+        <path d="M32 44V36H40L41 28H32V24H44L43 32L40 44L32 48L24 44L21 32H32V36H24L25 44L32 48L39 44L40 36H32Z" fill="white" />
+        <rect x="20" y="20" width="24" height="24" rx="4" stroke="white" strokeWidth="2" fill="none" />
+      </svg>
+    ),
+    title: 'HTML 5',
+    descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+  },
+  {
+    colorClass: sx.flagColor3,
+    icon: (
+      <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="16" y="16" width="32" height="32" rx="6" stroke="white" strokeWidth="4" fill="none" />
+        <path d="M24 40L32 24L40 40H24Z" fill="white" />
+      </svg>
+    ),
+    title: 'CSS 3',
+    descr: 'Lorem ipsum dolor sit.'
+  },
+  {
+    colorClass: sx.flagColor4,
+    icon: (
+      <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <rect x="16" y="16" width="32" height="32" rx="6" stroke="white" strokeWidth="4" fill="none" />
+        <rect x="24" y="24" width="16" height="16" fill="white" />
+        <text x="32" y="38" textAnchor="middle" fontSize="12" fill="#DA611E" fontWeight="bold">JS</text>
+      </svg>
+    ),
+    title: 'Javascript',
+    descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor laboriosam odio alias.'
+  },
+  {
+    colorClass: sx.flagColor5,
+    icon: (
+      <svg width="64" height="64" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="32" cy="32" r="24" stroke="white" strokeWidth="4" fill="none" />
+        <path d="M40 36C40 38.2091 36.4183 40 32 40C27.5817 40 24 38.2091 24 36" stroke="white" strokeWidth="3" />
+        <circle cx="32" cy="28" r="6" stroke="white" strokeWidth="2" />
+      </svg>
+    ),
+    title: 'Github',
+    descr: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.'
+  }
+];
 
 export default class ReactFlagTemplate extends React.Component<IReactFlagTemplateProps> {
-  public render(): React.ReactElement<IReactFlagTemplateProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
-
+  render() {
     return (
-      <section className={`${styles.reactFlagTemplate} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
-        </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
-        </div>
-      </section>
+      <div className={styles.flagBannerRoot}>
+        <h1>UL banner cards</h1>
+        <div className={styles.bannerBar}></div>
+        <ul className={styles.flagList}>
+          {flags.map((flag, idx) => (
+            <li key={idx} className={`${styles.flagListItem} ${flag.colorClass}`}>
+              <div className={styles.flag}>
+                <div className={styles.icon}>{flag.icon}</div>
+                <div className={styles.title}>{flag.title}</div>
+                <div className={styles.descr}>{flag.descr}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
