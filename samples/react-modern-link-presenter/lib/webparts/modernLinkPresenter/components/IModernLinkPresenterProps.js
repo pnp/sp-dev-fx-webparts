93f289ca3b33795c63804911da2cd02b8b926f2a
MarcSZ <77404661+maschroeder-z@users.noreply.github.com>
2025-10-18 17:26:29 +0200

    Removed lib

--- samples/react-modern-link-presenter/lib/webparts/modernLinkPresenter/components/IModernLinkPresenterProps.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=IModernLinkPresenterProps.js.map
--- samples/react-modern-link-presenter/lib/webparts/modernLinkPresenter/components/IconPicker.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-export interface IconPickerProps {
-    value?: string;
-    onChange: (iconName: string) => void;
-}
-export declare const IconPicker: React.FC<IconPickerProps>;
-//# sourceMappingURL=IconPicker.d.ts.map
