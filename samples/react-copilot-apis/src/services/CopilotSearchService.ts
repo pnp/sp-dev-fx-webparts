f3e69c7a2fb62e50e987f58c1819f7acc58ca098
Paolo Pialorsi <paolo.pialorsi@microsoft.com>
2025-12-08 23:30:08 +0100

    Added sample.json and preview image

--- samples/react-copilot-apis/src/services/CopilotSearchService.ts
+++
@@ -1,5 +0,0 @@
-// Import types for supporting SPFx with the service class
-import { ICopilotSearchService } from "./ICopilotSearchService";
-
-export class CopilotSearchService implements ICopilotSearchService {
-}
