deeade9c7aecc5e863da12623423b8138ef48f94
Sudeep Ghatak <sudeep.ghatak@gmail.com>
2026-08-26 11:06:19 +1200

    fix: replace global gulp with local for all scripts; add trust-dev-cert npm script; integrate spfx-fast-serve

--- samples/react-delivery-plan/gulpfile.js
+++ samples/react-delivery-plan/gulpfile.js
@@ -1,4 +1,10 @@
 'use strict';
 const build = require('@microsoft/sp-build-web');
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
+/* fast-serve */
+const { addFastServe } = require("spfx-fast-serve-helpers");
+addFastServe(build);
+/* end of fast-serve */
+
 build.initialize(require('gulp'));
+
